3b9e07cb83b67112f189ca0bcc270a80c25f806a
matt <>
2008-07-23 02:08:01 +0000

    *** empty log message ***

---
+++ Trunk_Jun_09_08/src/com/bagnet/nettracer/tracing/actions/WorldTracerSUSRITAction.java
@@ -0,0 +1,24 @@
+package com.bagnet.nettracer.tracing.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import com.bagnet.nettracer.tracing.constant.TracingConstants;
+import com.bagnet.nettracer.tracing.db.Agent;
+import com.bagnet.nettracer.tracing.utils.TracerUtils;
+
+public class WorldTracerSUSRITAction extends Action {
+   @Override
+public ActionForward execute(ActionMapping mapping, ActionForm form,
+		HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+	// TODO Auto-generated method stub
+	return mapping.findForward(TracingConstants.SUSPEND_RIH);
+}
+}
