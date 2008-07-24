98b2604e9f8b0ffa25be396aa1cc39e837094d5c
matt <>
2008-07-24 09:48:11 +0000

    *** empty log message ***

--- Trunk_Jun_09_08/src/com/bagnet/nettracer/tracing/actions/WorldTracerSUSRITAction.java
+++ Trunk_Jun_09_08/src/com/bagnet/nettracer/tracing/actions/WorldTracerSUSRITAction.java
@@ -1,5 +1,7 @@
 package com.bagnet.nettracer.tracing.actions;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,22 +12,26 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
 import org.apache.struts.util.MessageResources;
 
 import com.bagnet.nettracer.tracing.constant.TracingConstants;
 import com.bagnet.nettracer.tracing.db.Agent;
+import com.bagnet.nettracer.tracing.db.Incident;
+import com.bagnet.nettracer.tracing.db.Item;
+import com.bagnet.nettracer.tracing.db.OHD;
 import com.bagnet.nettracer.tracing.utils.TracerUtils;
 import com.bagnet.nettracer.tracing.utils.UserPermissions;
 
 public class WorldTracerSUSRITAction extends Action {
-   @Override
-public ActionForward execute(ActionMapping mapping, ActionForm form,
-		HttpServletRequest request, HttpServletResponse response)
-		throws Exception {
-	// TODO Auto-generated method stub
-	   HttpSession session = request.getSession();
-		
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+
 		// check session and user validity
 		TracerUtils.checkSession(session);
 		Agent user = (Agent) session.getAttribute("user");
@@ -33,42 +39,107 @@ public ActionForward execute(ActionMapping mapping, ActionForm form,
 			response.sendRedirect("logoff.do");
 			return null;
 		}
-		
-		if (!UserPermissions.hasPermission(TracingConstants.SYSTEM_COMPONENT_NAME_WORLD_TRACER_ROH, user))
+
+		if (!UserPermissions.hasPermission(
+				TracingConstants.SYSTEM_COMPONENT_NAME_WORLD_TRACER_ROH, user))
 			return (mapping.findForward(TracingConstants.NO_PERMISSION));
 
 		ActionMessages errors = new ActionMessages();
 
-		//Obtain a handle on the resources directory.
+		// Obtain a handle on the resources directory.
 		MessageResources messages = MessageResources
 				.getMessageResources("com.bagnet.nettracer.tracing.resources.ApplicationResources");
-		String suspend=request.getParameter("suspend");
-		String fileReference=request.getParameter("fileReference");
-		if(null==fileReference||"".equals(fileReference))
-		{
-			request.setAttribute("fileReference", "please put a fileReference");
+		String checkbox[]=request.getParameterValues("judgepartsuspend");
+		if(checkbox!=null){
+			TracerUtils.madePartSuspendWT_BAG_SELECTED(checkbox);
 		}
-		if(suspend.equals("completeSUS")){
-			String ahlORohd=request.getParameter("ahlORohd");
-			/**
-			List list=TracerUtils.getAhlOROhlProperty(ahlORohd, fileReference);
-			if(list.size()!=0){
-				request.setAttribute("error", "there is not a valid file reference");
+		String suspend = request.getParameter("suspend");
+		System.out.println("==============" + suspend);
+		String fileReference = request.getParameter("fileReference");
+		String ahlORohd = request.getParameter("ahlORohd");
+		List list3 = new ArrayList();
+		List list4 = new ArrayList();
+		request.setAttribute("radio", "completeSUS");
+		// fileReference.trim()
+		if (null == fileReference || "".equals(fileReference.trim())) {
+			ActionMessage error = new ActionMessage("error.noincident");
+			errors.add(ActionMessages.GLOBAL_MESSAGE, error);
+			saveMessages(request, errors);
+			request.setAttribute("filereference","");
+		} else {
+			// 判断item表中是否存在该fileReference
+			request.setAttribute("filereference",fileReference);
+			if (suspend.equals("completeSUS")) {// 如果是:complete类型的suspend
+				request.setAttribute("radio", "completeSUS");
+				if (ahlORohd.equals("ahl")) {
+					Incident incident = TracerUtils
+							.incidentFileReference(fileReference);
+					if (null == incident) {
+						ActionMessage error = new ActionMessage(
+								"error.noincident");
+						errors.add(ActionMessages.GLOBAL_MESSAGE, error);
+						saveMessages(request, errors);
+					} else {
+						if (incident.getWt_id() == null
+								|| incident.getWt_id().trim().equals("")) {
+							ActionMessage error = new ActionMessage(
+									"error.nowt_id");
+							errors.add(ActionMessages.GLOBAL_MESSAGE, error);
+							saveMessages(request, errors);
+						} else {
+							list3 = incident.getItemlist();
+							TracerUtils.madeSuspendWT_BAG_SELECTED(list3);
+							System.out.println("wwwwwwwwwwwwwwwwww"
+									+ list3.size());
+						}
+					}
+				} else if (ahlORohd.equals("ohd")) {
+					OHD ohd = TracerUtils.ohdFileReference(fileReference);
+					if (ohd == null) {
+						ActionMessage error = new ActionMessage("error.noohd");
+						errors.add(ActionMessages.GLOBAL_MESSAGE, error);
+						saveMessages(request, errors);
+					} else {
+						if (ohd.getWt_id() == null
+								|| ohd.getWt_id().trim().equals("")) {
+							ActionMessage error = new ActionMessage(
+									"error.nowt_id");
+							errors.add(ActionMessages.GLOBAL_MESSAGE, error);
+							saveMessages(request, errors);
+						} else {
+							list3 = (List) ohd.getItems();
+							TracerUtils.madeSuspendWT_BAG_SELECTED(list3);
+						}
+					}
+				}
+			} else if (suspend.equals("partSUS")) {// 如果是:part类型的suspend
+				request.setAttribute("radio", "partSUS");
+				Incident incident = TracerUtils
+						.incidentFileReference(fileReference);
+				if (null == incident) {
+					ActionMessage error = new ActionMessage("error.noincident");
+					errors.add(ActionMessages.GLOBAL_MESSAGE, error);
+					saveMessages(request, errors);
+				} else {
+					if (incident.getWt_id() == null
+							|| incident.getWt_id().trim().equals("")) {
+						ActionMessage error = new ActionMessage("error.nowt_id");
+						errors.add(ActionMessages.GLOBAL_MESSAGE, error);
+						saveMessages(request, errors);
+					} else {
+						list3 = incident.getItemlist();
+						Iterator it = list3.iterator();
+						while (it.hasNext()) {
+							Item item = (Item) it.next();
+							if (item.getWt_bag_selected() == 0) {
+								list4.add(item);
+							}
+						}
+						request.setAttribute("partresultlist", list4);
+					}
+				}
 			}
-			else{
-				request.setAttribute("resultlist", list);
-			}
-		}
-		else{
-			String ahlORohd="ahl";
-			List list=TracerUtils.getAhlOROhlProperty(ahlORohd, fileReference);
-			if(list.size()!=0){
-				request.setAttribute("error", "there is not a valid file reference");
-			}
-			else{
-				request.setAttribute("resultlist", list);
-			}*/
 		}
 		return mapping.findForward(TracingConstants.WORLDTRACER_SUS_RIT);
-   }
+	}
 }
