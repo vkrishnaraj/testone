82f011cbcd4f064a863aed6a198a5ad6154f347e
byron <>
2008-08-19 15:19:09 +0000

    *** empty log message ***

---
+++ Trunk_Jun_09_08/src/com/bagnet/nettracer/exceptions/BagtagException.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.bagnet.nettracer.exceptions;
+
+/**
+ * @author byron
+ *
+ */
+public class BagtagException extends Exception {
+
+	public static final String INVALID_FORMAT_MESSAGE = "The provided bag tag was not in an expected format."; 
+	public static final String NO_MATCH_MESSAGE = "No match was found for the provided airline code.";
+	/**
+	 * 
+	 */
+	public BagtagException() {
+	}
+
+	/**
+	 * @param message
+	 */
+	public BagtagException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public BagtagException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public BagtagException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
