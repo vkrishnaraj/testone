f8c59110e2ba0bd96e43ade1b891ce2e3591f0a8
byron <>
2008-12-18 21:26:40 +0000

    JetBlue

---
+++ Trunk_Jun_09_08/src/com/bagnet/clients/b6/JetBlueIntegrationWrapper.java
@@ -0,0 +1,197 @@
+package com.bagnet.clients.b6;
+
+import org.apache.log4j.Logger;
+
+import servicemanager.uddi_f5410361_cab6_11dd_b417_a18226e1080a.BaggageService_vs3_0Stub;
+
+import com.bagnet.nettracer.exceptions.BagtagException;
+import com.bagnet.nettracer.tracing.bmo.PropertyBMO;
+import com.bagnet.nettracer.tracing.utils.TracerProperties;
+import com.bagnet.nettracer.tracing.utils.lookup.LookupAirlineCodes;
+import com.jetblue.schemas._2008._03.framework.baggage.ReservationDetail;
+import com.jetblue.schemas._2008._03.framework.services.AuthorizationHeader;
+import com.jetblue.schemas._2008._03.framework.services.AuthorizationHeaderDocument;
+import com.jetblue.schemas._2008._03.framework.services.baggage.BagNumberRequest;
+import com.jetblue.schemas._2008._03.framework.services.baggage.BagNumberRequestDocument;
+import com.jetblue.schemas._2008._03.framework.services.baggage.PassengerDetailResponseDocument;
+import com.jetblue.schemas._2008._03.framework.services.baggage.RecordLocatorRequest;
+import com.jetblue.schemas._2008._03.framework.services.baggage.RecordLocatorRequestDocument;
+import com.jetblue.schemas._2008._03.framework.services.baggage.ReservationDetailResponseDocument;
+
+public class JetBlueIntegrationWrapper {
+
+	
+	private String errorMessage;
+	private String pnrContents;
+	
+	private Logger logger = Logger.getLogger(JetBlueIntegrationWrapper.class);
+	private String endpoint = PropertyBMO.getValue(PropertyBMO.PROPERTY_BOOKING_ENDPOINT);
+	private String consumerId = PropertyBMO.getValue("jetblue.reservation.consumerId");
+	private String signature = PropertyBMO.getValue("jetblue.reservation.signature");
+	private String token = PropertyBMO.getValue("jetblue.reservation.token");
+	private ReservationDetail reservationDetail = null;
+
+
+	
+	public boolean writeCommentToPNR(String recordLocator, String comment) {
+		if (TracerProperties.isTrue(TracerProperties.RESERVATION_UPDATE_COMMENT_ON)
+				&& recordLocator != null && recordLocator.trim().length() == 6) {
+			try {
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				setErrorMessage("Error calling webservice: " + e.toString());
+				return false;
+			}
+			return true;
+		}
+		return false;
+
+	}
+
+	/**
+	 * Method assumes that one of the two keys is null; it will perform the
+	 * reservation system baed on the first key that is not null.
+	 * 
+	 * @param recordLocator
+	 * @param bagTag
+	 * @return
+	 */
+	public boolean getBookingByKey(String recordLocator, String bagTag) {
+
+		try {
+			
+			if (recordLocator != null) {
+				reservationDetail = getBookingByPnr(recordLocator);
+			} else if (bagTag != null) {
+				bagTag = bagTag.toUpperCase();
+				try {
+					bagTag = LookupAirlineCodes.getFullBagTag(bagTag);
+				} catch (BagtagException e) {
+					// Ignore
+				}
+				reservationDetail = getBookingByTag(bagTag);
+			} else {
+				return false;
+			}
+
+		} catch (Exception e) {
+			logger.error("Exception thrown in JetBlue Integration Wrapper", e);
+			e.printStackTrace();
+			return false;
+		}
+		if (reservationDetail == null) {
+			return false;
+		}
+		return true;
+	}
+
+	private ReservationDetail getBookingByTag(String bagTag) {
+		try {
+			
+			AuthorizationHeaderDocument authDoc = getAuthorizationHeader();
+
+			BaggageService_vs3_0Stub stub = new BaggageService_vs3_0Stub(endpoint);
+			
+			BagNumberRequestDocument requestDoc = BagNumberRequestDocument.Factory.newInstance();
+			BagNumberRequest request = requestDoc.addNewBagNumberRequest();
+			request.setBagNumber(bagTag);
+			
+			PassengerDetailResponseDocument responseDoc = stub.GetPassengerDetailByBagNumber(requestDoc , authDoc);
+			
+			String recordLocator = responseDoc.getPassengerDetailResponse().getPassengerDetail().getRecordLocator();
+			
+
+			if (recordLocator != null) {
+				return getBookingByPnr(recordLocator);
+			}
+			
+		} catch (Exception e) {
+			logger.error("Exception thrown in JetBlue Integration Wrapper", e);
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+
+	private ReservationDetail getBookingByPnr(String recordLocator) {
+		try {
+			BaggageService_vs3_0Stub stub = new BaggageService_vs3_0Stub(endpoint);
+			
+			RecordLocatorRequestDocument requestDoc = RecordLocatorRequestDocument.Factory.newInstance();
+			RecordLocatorRequest recordRequest = requestDoc.addNewRecordLocatorRequest();
+			recordRequest.setRecordLocator(recordLocator);
+			
+			AuthorizationHeaderDocument authDoc = getAuthorizationHeader();
+			
+			ReservationDetailResponseDocument responseDoc = stub.GetReservationDetailByRecordLocator(requestDoc, authDoc);
+			return responseDoc.getReservationDetailResponse().getReservationDetail();
+		} catch (Exception e) {
+			logger.error("Exception thrown in JetBlue Integration Wrapper", e);
+			
+		}
+		return null;
+	}
+
+	private boolean getPnrContents(String recordLocator) {
+		try {
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			setErrorMessage("Error calling webservice: " + e.toString());
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	public Booking getBooking() {
+		return booking;
+	}
+
+	public void setBooking(Booking booking) {
+		this.booking = booking;
+	}
+	*/
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	private void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	/**
+	 * @return the pnrContents
+	 */
+	public String getPnrContents() {
+		return pnrContents;
+	}
+
+	/**
+	 * @param pnrContents
+	 *          the pnrContents to set
+	 */
+	public void setPnrContents(String pnrContents) {
+		this.pnrContents = pnrContents;
+	}
+	
+	private AuthorizationHeaderDocument getAuthorizationHeader() {
+		AuthorizationHeaderDocument authDoc = AuthorizationHeaderDocument.Factory.newInstance();
+		AuthorizationHeader header = authDoc.addNewAuthorizationHeader();
+		header.setConsumerId(consumerId);
+		header.setSignature(signature);
+		header.setToken(token);
+		return authDoc;
+	}
+
+	public ReservationDetail getReservationDetail() {
+		return reservationDetail;
+	}
+
+	public void setReservationDetail(ReservationDetail reservationDetail) {
+		this.reservationDetail = reservationDetail;
+	}
+}
---
+++ Trunk_Jun_09_08/src/com/bagnet/clients/b6/ReservationIntegrationImpl.java
@@ -0,0 +1,532 @@
+package com.bagnet.clients.b6;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Hashtable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.datacontract.schemas._2004._07.jetblue_framework_contracts_baggage.BagStatus;
+
+import com.bagnet.nettracer.integrations.reservation.ReservationIntegration;
+import com.bagnet.nettracer.tracing.bmo.StatusBMO;
+import com.bagnet.nettracer.tracing.constant.TracingConstants;
+import com.bagnet.nettracer.tracing.db.Agent;
+import com.bagnet.nettracer.tracing.db.Incident_Claimcheck;
+import com.bagnet.nettracer.tracing.db.Item;
+import com.bagnet.nettracer.tracing.db.Itinerary;
+import com.bagnet.nettracer.tracing.db.OHD_Itinerary;
+import com.bagnet.nettracer.tracing.forms.IncidentForm;
+import com.bagnet.nettracer.tracing.forms.OnHandForm;
+import com.jetblue.schemas._2008._03.framework.Address;
+import com.jetblue.schemas._2008._03.framework.FlightKey;
+import com.jetblue.schemas._2008._03.framework.Name;
+import com.jetblue.schemas._2008._03.framework.Phone;
+import com.jetblue.schemas._2008._03.framework.baggage.FlightBag;
+import com.jetblue.schemas._2008._03.framework.baggage.PassengerDetail;
+import com.jetblue.schemas._2008._03.framework.baggage.PassengerFlight;
+import com.jetblue.schemas._2008._03.framework.baggage.ReservationDetail;
+
+public class ReservationIntegrationImpl extends
+		com.bagnet.clients.defaul.ReservationIntegrationImpl implements
+		ReservationIntegration {
+	private Logger logger = Logger.getLogger(ReservationIntegrationImpl.class);
+	private ReservationDetail booking = null;
+	
+	private final int HOURS_BACK_ITINERARY = 48;
+	private final int HOURS_FORWARD_ITINERARY = 24;
+	private String pnrContents;
+
+
+	private ArrayList<String> getBooking(HttpServletRequest request,
+			IncidentForm form) {
+		ArrayList<String> errors = new ArrayList<String>();
+		try {
+			JetBlueIntegrationWrapper wrapper = new JetBlueIntegrationWrapper();
+			
+			boolean result = true;
+			if (form.getRecordlocator() != null && form.getRecordlocator().trim().length() > 0) {
+				// If searching bag record locator				
+				result = wrapper.getBookingByKey(form.getRecordlocator(), null);
+			} else if (form.getBagTagNumber() != null && form.getBagTagNumber().trim().length() > 0) {
+				// If searching by bag tag number
+				result = wrapper.getBookingByKey(null, form.getBagTagNumber());
+			} else {
+				// Neither a bag tag or record locator was provided.
+				if (wrapper.getErrorMessage() != null) {
+					addError(errors, wrapper.getErrorMessage());
+				} else {
+					addError(errors, "error.no.recordlocator.bagtag");
+				}
+					
+				return errors;
+			}
+			
+			if (!result) {
+				addError(errors, "error.no.recordlocator.bagtag");
+			} else {
+				// Get the booking
+				booking = wrapper.getReservationDetail();
+			}
+
+			return errors;
+
+		} catch (Exception e) {
+			logger.error(e.getStackTrace());
+			e.printStackTrace();
+			return errors;
+		}
+	}
+	
+	private ArrayList<String> getBooking(HttpServletRequest request,
+			OnHandForm form) {
+		ArrayList<String> errors = new ArrayList<String>();
+		try {
+			JetBlueIntegrationWrapper wrapper = new JetBlueIntegrationWrapper();
+			// Record Locator
+			
+			boolean result = true;
+			if (form.getRecordlocator() != null && form.getRecordlocator().trim().length() > 0) {
+				// If searching bag record locator				
+				result = wrapper.getBookingByKey(form.getRecordlocator(), null);
+			} else if (form.getBagTagNumber() != null && form.getBagTagNumber().trim().length() > 0) {
+				// If searching by bag tag number
+				result = wrapper.getBookingByKey(null, form.getBagTagNumber());
+			} else {
+				// Neither a bag tag or record locator was provided.
+				addError(errors, "error.no.recordlocator.bagtag");
+				return errors;
+			}
+			
+			if (!result) {
+				if (wrapper.getErrorMessage() != null) {
+					addError(errors, wrapper.getErrorMessage());
+				} else {
+					addError(errors, "error.no.recordlocator.bagtag");	
+				}
+			} else {
+				// Get the booking
+				booking = wrapper.getReservationDetail();
+			}
+
+			return errors;
+
+		} catch (Exception e) {
+			logger.error(e.getStackTrace());
+			e.printStackTrace();
+			return errors;
+		}
+	}
+	
+	public ArrayList<String> populateIncidentForm(HttpServletRequest request,
+			IncidentForm form, int incidentType) {
+		
+		ArrayList<String> retList = getBooking(request, form);
+		
+		if (retList.size() > 0) {
+			return retList;
+		}
+		
+		form = (IncidentForm) request.getSession().getAttribute("incidentForm");
+		populateIncidentFormInner(form, incidentType, request);
+		HttpSession session = request.getSession();
+		session.setAttribute("incidentForm", form);
+		return retList;
+	}
+	
+	public ArrayList<String> populateOhdForm(HttpServletRequest request,
+			OnHandForm form) {
+
+		
+		ArrayList<String> retList = getBooking(request, form);
+		
+		if (retList.size() > 0) {
+			return retList;
+		}
+			
+		populateOhdFormInner(request, form);
+		HttpSession session = request.getSession();
+		session.setAttribute("OnHandForm", form);
+		return retList;
+
+	}
+
+	
+	private void populateOhdFormInner(HttpServletRequest request, OnHandForm form) {
+		
+		form.setPnr(booking.getRecordLocator());
+		Agent user = (Agent) request.getSession().getAttribute("user");
+		form.setHolding_company(user.getCompanycode_ID());
+		form.setHolding_station(user.getStation().getStationcode());
+
+		int bagIndex = 0;
+		int itinCount = 0;
+		int paxItinCount = 0;
+		int bagItinCount = 0;
+		Hashtable<String, Integer> bagTable = new Hashtable<String, Integer>();
+		
+		String email = booking.getContactEmail();
+		Name contactName = booking.getContactName();
+		String contactNameString = contactName.getFirstName() + " " + contactName.getLastName();
+		Address address = booking.getContactAddress();
+		
+		String homePhone = null;
+		String faxPhone = null;
+		String otherPhone = null;
+		if (booking.getContactPhoneNumbers().getPhoneArray() != null) {
+			for (Phone phone: booking.getContactPhoneNumbers().getPhoneArray()) {
+				if (phone.getPhoneType().equals("HOME")) {
+					homePhone = phone.getNumber();
+				} else if (phone.getPhoneType().equals("FAX")) {
+					faxPhone = phone.getNumber();
+				} else if (phone.getPhoneType().equals("OTHER")) {
+					otherPhone = phone.getNumber();
+				}
+				
+			}
+		}
+		
+		// Pasengers and Bags
+		if (booking.getPassengerDetails() != null) {
+			PassengerDetail[] paxArray = booking.getPassengerDetails().getPassengerDetailArray();
+			
+			
+			
+			if (paxArray != null && paxArray.length > 0) {
+				int paxNumber = 0;
+				int baggageRoutingCount = 0;
+				
+				
+				for (PassengerDetail pax: paxArray) {
+					com.bagnet.nettracer.tracing.db.OHD_Passenger fPax = form.getPassenger(paxNumber);
+					
+					fPax.setFirstname(pax.getPassengerName().getFirstName());
+					fPax.setMiddlename(pax.getPassengerName().getMiddleName());
+					fPax.setLastname(pax.getPassengerName().getLastName());
+					
+					String fullName = pax.getPassengerName().getFirstName() + " " + pax.getPassengerName().getLastName();
+					if (fullName.equalsIgnoreCase(contactNameString) && address != null) {
+						
+						com.bagnet.nettracer.tracing.db.OHD_Address fAddr = fPax.getAddress(0);
+						
+						if (email != null) {
+							fAddr.setEmail(email);
+						}
+						
+						fAddr.setAddress1(address.getLine1());
+
+						String addressLine2 = "";
+						if (address.getLine2() != null) {
+							addressLine2 = address.getLine2();
+						}
+						
+						if (address.getLine3() != null) {
+							addressLine2 += " " + address.getLine3();
+						}
+						fAddr.setAddress2(addressLine2.trim());
+						fAddr.setCity(address.getCity());
+						fAddr.setZip(address.getPostalCode());
+						fAddr.setCountrycode_ID(address.getCountry());
+						if (address.getCountry() == null || address.getCountry().equals("US")) {
+							fAddr.setState_ID(address.getStateProvince());
+						} else {
+							fAddr.setProvince(address.getStateProvince());
+						}
+						
+						if (homePhone != null) {
+							fAddr.setHomephone(homePhone);
+						}
+						
+						if (faxPhone != null) {
+							fAddr.setAltphone(faxPhone);
+						}
+						
+						if (otherPhone != null) {
+							fAddr.setMobile(otherPhone);
+						}
+					}
+					
+
+					if (pax.getPassengerFlightDetails() != null) {
+						PassengerFlight[] flightArray = pax.getPassengerFlightDetails().getPassengerFlightArray();
+					
+						if (flightArray != null && flightArray.length > 0) {
+							for (PassengerFlight flight: flightArray) {
+								FlightKey key = flight.getFlightKey();
+								Calendar flightDate = key.getFlightDate();
+								
+								Long deptime = (flightDate.getTime().getTime()) / 3600000;
+								Long nowtime = ((new Date()).getTime()) / 3600000;
+								Long timeDifference = nowtime - deptime;
+								
+								boolean useSegment = false;
+								if (timeDifference <= HOURS_BACK_ITINERARY && timeDifference >= -HOURS_FORWARD_ITINERARY) {
+									useSegment = true;
+								}
+								
+								// If > -48 hours && < 24 hours
+								if (useSegment) {
+									
+									FlightBag[] flightBags = flight.getBagNumbers().getFlightBagArray();
+									if (flightBags != null && flightBags.length > 0)  {
+										for (FlightBag bag: flightBags) {
+											
+											if (bag.getStatus().equals(BagStatus.CHECKED_IN) && !bagTable.containsKey(bag.getBagNumber())) {
+												bagTable.put(bag.getBagNumber(), new Integer(0));
+												
+												bagIndex++;
+												if (paxNumber==0 && baggageRoutingCount == 0) {
+													
+													OHD_Itinerary bagItin = form.getItinerary(itinCount);
+													++itinCount;
+													++bagItinCount;
+													
+													bagItin.setDepartdate(flightDate.getTime());
+													bagItin.setAirline(key.getAirlineCode());
+													bagItin.setFlightnum(key.getFlightNumber());
+													bagItin.setLegfrom(key.getDepartureAirport());
+													bagItin.setLegto(key.getArrivalAirport());
+												}
+											}
+										}
+									}
+								}
+							}
+						} 
+					}
+					
+					++paxNumber;
+				}				
+			}
+			
+			if (bagItinCount == 0) {
+				OHD_Itinerary bagItin = form.getItinerary(itinCount);
+				++itinCount;
+				++bagItinCount;
+			}
+		}
+	}
+	
+
+
+	public ArrayList<String> writeCommentToPNR(String comment,
+			String recordLocator) {
+
+		/*
+		SharesIntegrationWrapper wrapper = new SharesIntegrationWrapper();
+		wrapper.writeCommentToPNR(recordLocator, comment);
+		return new ArrayList<String>();
+		*/
+		return null;
+	}
+
+	private void populateIncidentFormInner(IncidentForm form,
+			int itemtype, HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+		Agent user = (Agent) session.getAttribute("user");
+		
+		// Record Locator
+		form.setRecordlocator(booking.getRecordLocator());
+		form.setAgent(user);
+		if (pnrContents != null) {
+			form.setOtherSystemInformation(pnrContents);
+		}
+		int bagIndex = 0;
+		int itinCount = 0;
+		int paxItinCount = 0;
+		int bagItinCount = 0;
+		Hashtable<String, Integer> bagTable = new Hashtable<String, Integer>();
+		
+		String email = booking.getContactEmail();
+		Name contactName = booking.getContactName();
+		String contactNameString = contactName.getFirstName() + " " + contactName.getLastName();
+		Address address = booking.getContactAddress();
+		
+		String homePhone = null;
+		String faxPhone = null;
+		String otherPhone = null;
+		if (booking.getContactPhoneNumbers().getPhoneArray() != null) {
+			for (Phone phone: booking.getContactPhoneNumbers().getPhoneArray()) {
+				if (phone.getPhoneType().equals("HOME")) {
+					homePhone = phone.getNumber();
+				} else if (phone.getPhoneType().equals("FAX")) {
+					faxPhone = phone.getNumber();
+				} else if (phone.getPhoneType().equals("OTHER")) {
+					otherPhone = phone.getNumber();
+				}
+				
+			}
+		}
+		
+		
+
+		// Pasengers and Bags
+		if (booking.getPassengerDetails() != null) {
+			PassengerDetail[] paxArray = booking.getPassengerDetails().getPassengerDetailArray();
+			
+			
+			
+			if (paxArray != null && paxArray.length > 0) {
+				form.setNumpassengers(paxArray.length);
+				int paxNumber = 0;
+				int baggageRoutingCount = 0;
+				
+				
+				for (PassengerDetail pax: paxArray) {
+					com.bagnet.nettracer.tracing.db.Passenger fPax = form.getPassenger(paxNumber);
+					
+					fPax.setFirstname(pax.getPassengerName().getFirstName());
+					fPax.setMiddlename(pax.getPassengerName().getMiddleName());
+					fPax.setLastname(pax.getPassengerName().getLastName());
+					
+					String fullName = pax.getPassengerName().getFirstName() + " " + pax.getPassengerName().getLastName();
+					if (fullName.equalsIgnoreCase(contactNameString) && address != null) {
+						
+						com.bagnet.nettracer.tracing.db.Address fAddr = fPax.getAddress(0);
+						
+						if (email != null) {
+							fAddr.setEmail(email);
+						}
+						
+						fAddr.setAddress1(address.getLine1());
+
+						String addressLine2 = "";
+						if (address.getLine2() != null) {
+							addressLine2 = address.getLine2();
+						}
+						
+						if (address.getLine3() != null) {
+							addressLine2 += " " + address.getLine3();
+						}
+						fAddr.setAddress2(addressLine2.trim());
+						fAddr.setCity(address.getCity());
+						fAddr.setZip(address.getPostalCode());
+						fAddr.setCountrycode_ID(address.getCountry());
+						if (address.getCountry() == null || address.getCountry().equals("US")) {
+							fAddr.setState_ID(address.getStateProvince());
+						} else {
+							fAddr.setProvince(address.getStateProvince());
+						}
+						
+						if (homePhone != null) {
+							fAddr.setHomephone(homePhone);
+						}
+						
+						if (faxPhone != null) {
+							fAddr.setAltphone(faxPhone);
+						}
+						
+						if (otherPhone != null) {
+							fAddr.setMobile(otherPhone);
+						}
+					}
+					
+
+					if (pax.getPassengerFlightDetails() != null) {
+						PassengerFlight[] flightArray = pax.getPassengerFlightDetails().getPassengerFlightArray();
+					
+						if (flightArray != null && flightArray.length > 0) {
+							for (PassengerFlight flight: flightArray) {
+								FlightKey key = flight.getFlightKey();
+								Calendar flightDate = key.getFlightDate();
+								
+								Long deptime = (flightDate.getTime().getTime()) / 3600000;
+								Long nowtime = ((new Date()).getTime()) / 3600000;
+								Long timeDifference = nowtime - deptime;
+								
+								boolean useSegment = false;
+								if (timeDifference <= HOURS_BACK_ITINERARY && timeDifference >= -HOURS_FORWARD_ITINERARY) {
+									useSegment = true;
+								}
+								
+								// If > -48 hours && < 24 hours
+								if (useSegment) {
+									
+									if (paxNumber == 0) { 
+										Itinerary itin = form.getItinerary(itinCount, TracingConstants.PASSENGER_ROUTING);
+										++itinCount;
+										++paxItinCount;
+										
+										itin.setDepartdate(flightDate.getTime());
+										itin.setAirline(key.getAirlineCode());
+										itin.setFlightnum(key.getFlightNumber());
+										itin.setLegfrom(key.getDepartureAirport());
+										itin.setLegto(key.getArrivalAirport());
+									}
+									
+									
+									FlightBag[] flightBags = flight.getBagNumbers().getFlightBagArray();
+									if (flightBags != null && flightBags.length > 0)  {
+										for (FlightBag bag: flightBags) {
+											
+											if (bag.getStatus().equals(BagStatus.CHECKED_IN) && !bagTable.containsKey(bag.getBagNumber())) {
+												bagTable.put(bag.getBagNumber(), new Integer(0));
+												Incident_Claimcheck ic = form.getClaimcheck(bagIndex);
+												ic.setClaimchecknum(bag.getBagNumber());
+							
+												// bag info
+												Item theitem = form.getItem(bagIndex, itemtype);
+												if (itemtype != TracingConstants.LOST_DELAY
+														&& bag.getBagNumber() != null && bag.getBagNumber().length() > 0) {
+													theitem.setClaimchecknum(bag.getBagNumber());
+												}
+												
+												theitem.set_DATEFORMAT(user.getDateformat().getFormat());
+												theitem.setCurrency_ID(user.getDefaultcurrency());
+												theitem.setXdescelement_ID_1(TracingConstants.XDESC_TYPE_X);
+												theitem.setXdescelement_ID_2(TracingConstants.XDESC_TYPE_X);
+												theitem.setXdescelement_ID_3(TracingConstants.XDESC_TYPE_X);
+												theitem.setBagnumber(bagIndex);
+												theitem.setStatus(StatusBMO.getStatus(
+														TracingConstants.ITEM_STATUS_OPEN, user.getCurrentlocale()));
+												
+												bagIndex++;
+												form.setNumbagchecked(bagIndex);												
+
+												if (paxNumber==0 && baggageRoutingCount == 0) {
+													
+
+													Itinerary bagItin = form.getItinerary(itinCount, TracingConstants.BAGGAGE_ROUTING);
+													++itinCount;
+													++bagItinCount;
+													
+													bagItin.setDepartdate(flightDate.getTime());
+													bagItin.setAirline(key.getAirlineCode());
+													bagItin.setFlightnum(key.getFlightNumber());
+													bagItin.setLegfrom(key.getDepartureAirport());
+													bagItin.setLegto(key.getArrivalAirport());
+												}
+											}
+										}
+									}
+								}
+							}
+						} 
+					}
+					
+					++paxNumber;
+				}				
+			} else {
+				form.setNumpassengers(1);
+				form.getPassenger(0);
+			}
+			
+			if (paxItinCount == 0) {
+				Itinerary bagItin = form.getItinerary(itinCount, TracingConstants.PASSENGER_ROUTING);
+				++itinCount;
+				++bagItinCount;
+			}
+			
+			if (bagItinCount == 0) {
+				Itinerary bagItin = form.getItinerary(itinCount, TracingConstants.BAGGAGE_ROUTING);
+				++itinCount;
+				++bagItinCount;
+			}
+			
+		}
+	}
+}
