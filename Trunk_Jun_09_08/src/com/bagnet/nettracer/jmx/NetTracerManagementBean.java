908d8d551e2cddb765ee05cf0890fda0cbe78470
noah <>
2009-05-06 19:51:16 +0000

    *** empty log message ***

--- Trunk_Jun_09_08/src/com/bagnet/nettracer/jmx/NetTracerManagementBean.java
+++ Trunk_Jun_09_08/src/com/bagnet/nettracer/jmx/NetTracerManagementBean.java
@@ -1,12 +1,5 @@
 package com.bagnet.nettracer.jmx;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Locale;
-import java.util.ResourceBundle;
-
-
 import com.bagnet.nettracer.tracing.bmo.PropertyBMO;
 import com.bagnet.nettracer.tracing.utils.TracerProperties;
 
@@ -19,14 +12,5 @@ public class NetTracerManagementBean {
 	public void reloadTracerFileProperties() {
 		TracerProperties.reloadProperties();
 	}
-	
-	public boolean reloadApplicationResources() {
-		ClassLoader cl = ClassLoader.getSystemClassLoader();
-		try {
-			ResourceBundle.clearCache(cl);
-			return true;
-		} catch (Exception e) {
-			return false;
-		} 
-	}
+
 }
