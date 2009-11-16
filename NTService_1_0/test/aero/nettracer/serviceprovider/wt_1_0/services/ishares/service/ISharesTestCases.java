0049021d3a81c346964d0bf40abbe48b6c072c20
byron <>
2009-11-16 21:48:40 +0000

    *** empty log message ***

--- NTService_1_0/test/aero/nettracer/serviceprovider/wt_1_0/services/ishares/service/ISharesTestCases.java
+++ NTService_1_0/test/aero/nettracer/serviceprovider/wt_1_0/services/ishares/service/ISharesTestCases.java
@@ -12,6 +12,8 @@ import aero.nettracer.serviceprovider.common.db.User;
 import aero.nettracer.serviceprovider.common.exceptions.UserNotAuthorizedException;
 import aero.nettracer.serviceprovider.common.utils.ServiceUtilities;
 import aero.nettracer.serviceprovider.wt_1_0.common.ActionFileRequestData;
+import aero.nettracer.serviceprovider.wt_1_0.common.Pxf;
+import aero.nettracer.serviceprovider.wt_1_0.common.PxfDetails;
 import aero.nettracer.serviceprovider.wt_1_0.common.WorldTracerResponse;
 import aero.nettracer.serviceprovider.wt_1_0.dto.WorldTracerActionDTO;
 import aero.nettracer.serviceprovider.wt_1_0.dto.WorldTracerActionType;
@@ -68,6 +70,42 @@ public class ISharesTestCases {
 
 	}
 	
+	@Test
+	public void testPlaceActionFileFile()	throws CommandNotProperlyFormedException, HttpException, IOException {
+		
+		// Create Payload & Type
+		WorldTracerActionType type =  WorldTracerActionType.ACTION_FILE_COUNTS;
+		Pxf payload = new Pxf();
+		payload.setContent("TEXT CONTENT");
+		payload.setDestination(1);
+		payload.setSendingStation("XAX");
+		
+		PxfDetails details1 = new PxfDetails();
+		details1.setStation("XLF");
+		details1.setArea("AP");
+		details1.setAirline("US");
+		
+		PxfDetails details2 = new PxfDetails();
+		details2.setStation("XAX");
+		details2.setArea("AA");
+		details2.setAirline("US");
+		
+		PxfDetails[] d = new PxfDetails[2];
+		d[0] = details1;
+		d[1] = details2;
+		payload.setPxfDetails(d);
+		
+		// Initialize other data (do not change)
+		WorldTracerResponse response = new WorldTracerResponse();
+		WorldTracerActionDTO dto = new WorldTracerActionDTO(type, null, payload, true, null);
+		WorldTracerServiceImpl impl = new WorldTracerServiceImpl(dto, true);
+		
+		// Perform Action
+		impl.placeActionFile(dto, payload, response);
+		
+		// Case-Specific Test of Data (Also use debugger to review contents of the "response" object.
+		int i = 0;
+	}
 	
 	
 	
