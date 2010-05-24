da1659aa273be83a624dcf65cf181a6b2d5118d9
olivetech <>
2010-05-24 15:11:28 +0000

    updated for Maintain Content and Language

---
+++ onlineclaims/src/main/java/com/nettracer/claims/core/model/Languages.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.nettracer.claims.core.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+
+/**
+ * @author Utpal
+ * 
+ */
+@Entity
+@Table(name="languages", uniqueConstraints={@UniqueConstraint(columnNames={"languageName"})})
+public class Languages  implements Serializable{
+
+	private static final long serialVersionUID = 8917820665547161835L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String languageName;
+	
+	
+	private String description;
+	
+	
+	private Boolean activeStatus;
+	
+	
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLanguageName() {
+		return languageName;
+	}
+
+	public void setLanguageName(String languageName) {
+		this.languageName = languageName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getActiveStatus() {
+		return activeStatus;
+	}
+
+	public void setActiveStatus(Boolean activeStatus) {
+		this.activeStatus = activeStatus;
+	}
+
+	
+	
+	
+}
