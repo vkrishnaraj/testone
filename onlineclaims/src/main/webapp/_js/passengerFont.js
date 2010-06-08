6fb3f00a57f0c9afb14d915595765708dda5d45c
olivetech <>
2010-06-08 13:50:16 +0000

    added for font size control in passenger module

---
+++ onlineclaims/src/main/webapp/_js/passengerFont.js
@@ -0,0 +1,174 @@
+function decreaseFontSize() {
+	
+   var p = document.getElementsByTagName('span');
+   
+   for(i=0;i<p.length;i++) {
+      if(p[i].style.fontSize) {
+         var s = parseInt(p[i].style.fontSize.replace("pt",""));
+         s=8;
+      } else {
+         var s = 8;
+      }
+      p[i].style.fontSize = s+"pt";
+   }   
+   
+   var input = document.getElementsByTagName('input');
+   for(i=0;i<input.length;i++) {
+	      if(input[i].style.fontSize) {
+	         var s = parseInt(input[i].style.fontSize.replace("pt",""));
+	         s=8;
+	      } else {
+	         var s = 8;
+	      }
+	      input[i].style.fontSize = s+"pt";
+	   } 
+   
+   var a = document.getElementsByTagName('a');
+   for(i=0;i<a.length;i++) {
+	      if(a[i].style.fontSize) {
+	         var s = parseInt(a[i].style.fontSize.replace("pt",""));
+	         s=8;
+	      } else {
+	         var s = 8;
+	      }
+	      a[i].style.fontSize = s+"pt";
+	   } 
+  
+   var para = document.getElementsByTagName('p');
+   for(i=0;i<para.length;i++) {
+	      if(para[i].style.fontSize) {
+	         var s = parseInt(para[i].style.fontSize.replace("pt",""));
+	         s=8;
+	      } else {
+	         var s = 8;
+	      }
+	      para[i].style.fontSize = s+"pt";
+	   } 
+   setId();
+   
+}
+
+
+	function mediumFontSize() {
+	   var p = document.getElementsByTagName('span');
+	   for(i=0;i<p.length;i++) {
+		   if(p[i].style.fontSize) {
+		         var s = parseInt(p[i].style.fontSize.replace("pt",""));
+		         s=10;
+		      } else {
+		         var s = 10;
+		      }
+		      
+		      p[i].style.fontSize = s+"pt";
+	   }
+	   
+	   var input = document.getElementsByTagName('input');
+	   for(i=0;i<input.length;i++) {
+		      if(input[i].style.fontSize) {
+		         var s = parseInt(input[i].style.fontSize.replace("pt",""));
+		         s=10;
+		      } else {
+		         var s = 10;
+		      }
+		      input[i].style.fontSize = s+"pt";
+		   } 
+	   
+	   var a = document.getElementsByTagName('a');
+	   for(i=0;i<a.length;i++) {
+		      if(a[i].style.fontSize) {
+		         var s = parseInt(a[i].style.fontSize.replace("pt",""));
+		         s=10;
+		      } else {
+		         var s = 10;
+		      }
+		      a[i].style.fontSize = s+"pt";
+		   } 
+	   
+	   var para = document.getElementsByTagName('p');
+	   for(i=0;i<para.length;i++) {
+		      if(para[i].style.fontSize) {
+		         var s = parseInt(para[i].style.fontSize.replace("pt",""));
+		         s=10;
+		      } else {
+		         var s = 10;
+		      }
+		      para[i].style.fontSize = s+"pt";
+		   } 
+	   
+	   setId();
+	}
+
+
+
+
+function increaseFontSize() {
+	   var p = document.getElementsByTagName('span');
+	   for(i=0;i<p.length;i++) {
+	      if(p[i].style.fontSize) {
+	         var s = parseInt(p[i].style.fontSize.replace("pt",""));
+	         s = 12;
+	      } else {
+	         var s = 12;
+	      }
+	      
+	      p[i].style.fontSize = s+"pt";
+	   }
+	   
+	   var input = document.getElementsByTagName('input');
+	   for(i=0;i<input.length;i++) {
+		      if(input[i].style.fontSize) {
+		         var s = parseInt(input[i].style.fontSize.replace("pt",""));
+		         s=12;
+		      } else {
+		         var s = 12;
+		      }
+		      input[i].style.fontSize = s+"pt";
+		   } 
+	   
+	   var a = document.getElementsByTagName('a');
+	   for(i=0;i<a.length;i++) {
+		      if(a[i].style.fontSize) {
+		         var s = parseInt(a[i].style.fontSize.replace("pt",""));
+		         s=12;
+		      } else {
+		         var s = 12;
+		      }
+		      a[i].style.fontSize = s+"pt";
+		   } 
+	   
+	   var para = document.getElementsByTagName('p');
+	   for(i=0;i<para.length;i++) {
+		      if(para[i].style.fontSize) {
+		         var s = parseInt(para[i].style.fontSize.replace("pt",""));
+		         s=12;
+		      } else {
+		         var s = 12;
+		      }
+		      para[i].style.fontSize = s+"pt";
+		   } 
+	   
+	   setId();
+	}
+
+
+	function setId(){
+			var id1 = document.getElementById("font1");
+		   var id2 = document.getElementById("font2");
+		   var id3 = document.getElementById("font3");
+		   
+		   var a = parseInt(id1.style.fontSize.replace("pt",""));
+		   a=8;
+		   
+		   var b = parseInt(id2.style.fontSize.replace("pt",""));
+		   b=10;
+		   
+		   var c = parseInt(id3.style.fontSize.replace("pt",""));
+		   c=12;
+		   
+		   id1.style.fontSize = a+"pt";
+		   id2.style.fontSize = b+"pt";
+		   id3.style.fontSize = c+"pt";
+		   
+		  
+	
+	}
