81aa752b4f3934e3e229bb546229bb6cb049ff1e
olivetech <>
2010-06-19 10:32:17 +0000

    updated for passenger module --passenger info page

--- onlineclaims/src/main/java/com/nettracer/claims/passenger/controller/PassengerController.java
+++ onlineclaims/src/main/java/com/nettracer/claims/passenger/controller/PassengerController.java
@@ -1,11 +1,14 @@
 package com.nettracer.claims.passenger.controller;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.faces.application.ViewHandler;
 import javax.faces.component.UIViewRoot;
 import javax.faces.component.html.HtmlForm;
 import javax.faces.component.html.HtmlInputSecret;
@@ -14,7 +17,6 @@ import javax.faces.component.html.HtmlPanelGrid;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.axis2.AxisFault;
@@ -24,12 +26,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.bagnet.nettracer.ws.core.pojo.xsd.WSPVAdvancedIncident;
-import com.bagnet.nettracer.ws.core.pojo.xsd.WSPVPassenger;
-import com.bagnet.nettracer.ws.onlineclaims.xsd.PassengerView;
-import com.nettracer.claims.admin.bootstrap.PassengerBootstrap;
-import com.nettracer.claims.core.exception.SimplePersistenceException;
-import com.nettracer.claims.core.model.Languages;
+import com.nettracer.claims.core.model.Bag;
+import com.nettracer.claims.core.model.CountryCode;
 import com.nettracer.claims.core.model.Localetext;
 import com.nettracer.claims.core.model.MultilingualLabel;
 import com.nettracer.claims.core.model.PassengerBean;
@@ -37,7 +35,6 @@ import com.nettracer.claims.core.service.PassengerService;
 import com.nettracer.claims.faces.util.CaptchaBean;
 import com.nettracer.claims.faces.util.FacesUtil;
 import com.nettracer.claims.passenger.LoginBean;
-import com.nettracer.claims.passenger.SessionPassengerBean;
 import com.nettracer.claims.webservices.client.OnlineClaimsWS;
 
 /**
@@ -46,7 +43,7 @@ import com.nettracer.claims.webservices.client.OnlineClaimsWS;
  */
 
 @Component
-@Scope("request")
+@Scope("session")
 @Qualifier("passengerController")
 public class PassengerController {
 	private static Logger logger = Logger.getLogger(PassengerController.class);
@@ -60,11 +57,14 @@ public class PassengerController {
 	//private List<Map<String, List<Localetext>>> pageMapsList ;
 	private Map<String, List<Localetext>> pageMaps ;
 	private List<Localetext> loginPageList;
-	private String selectedLanguage="English-US"; //holds the dropdown value for language selection
 	private Set<SelectItem> languageDropDown = new LinkedHashSet<SelectItem>();
 	private List<Localetext> passengerDirectionList;
 	private MultilingualLabel passengerInfoLabel;
 	private MultilingualLabel flightLabel;
+	private List<SelectItem> selectItems=new ArrayList<SelectItem>();
+	private Set<SelectItem> lostBagItems=new HashSet<SelectItem>();
+	private boolean differentClaimCheckView;
+	
 	
 	/*@Autowired
 	AdminService adminService;*/
@@ -75,109 +75,9 @@ public class PassengerController {
 	@Autowired
 	OnlineClaimsWS onlineClaimsWS;
 	
-	public PassengerController(){
-		logger.info("PassengerController constructor");
-		List<Languages> languagesList=PassengerBootstrap.getLanguageDropDown();
-		for(Languages language:languagesList){
-			if (language.getActiveStatus() == true){
-				languageDropDown.add(new SelectItem(language.getDescription()));
-			}
-		}
-		loginPageList= PassengerBootstrap.getLoginPageList();
-		logger.info("Size of loginPageList inside PassengerController constructor= "+loginPageList.size());
 
-	}
 
 	
-	
-	
-	public void languageSelectionListener(ValueChangeEvent valueChangeEvent){
-		logger.info("Listener:languageSelectionListener is called");
-		try {
-			selectedLanguage = (String) valueChangeEvent.getNewValue();
-			/*HttpSession session = (HttpSession) FacesUtil.getFacesContext()
-					.getExternalContext().getSession(false);*/
-			loginPageList=passengerService.getPassengerLoginContents(selectedLanguage);
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	
-
-	/**
-	 * If the validation would successful for login page then navigate to 2nd
-	 * page. i.e. the landing page
-	 * 
-	 * @return String
-	 * @throws AxisFault 
-	 */
-	public String gotoDirectionPage() {
-		logger.debug("gotoDirectionPage method is called");
-		 try {
-			PassengerView passengerView= onlineClaimsWS.getPassengerView(loginBean.getClaimNumber(), loginBean.getLastName());
-		
-		if (passengerView.getAuthenticationSuccess()) {
-			if (captchaBean.check().equalsIgnoreCase(CAPTCHA_STATUS)) {
-				FacesContext context = FacesUtil.getFacesContext();
-				HttpSession session = (HttpSession) context
-						.getExternalContext().getSession(false);
-				SessionPassengerBean sessionPassengerBean = (SessionPassengerBean) session
-						.getAttribute("sessionPassengerBean");
-				sessionPassengerBean.setLogoutRenderer(true);
-				session.setAttribute("sessionPassengerBean", sessionPassengerBean);
-				session.setAttribute("loggedPassenger", "loggedPassenger");
-				
-				baggageState=passengerView.getClaimId();
-				passengerDirectionList=passengerService.getPassengerDirection(selectedLanguage);
-				session.setAttribute("passengerDirectionList", passengerDirectionList);
-				
-				WSPVAdvancedIncident passengerData=passengerView.getData();
-				WSPVPassenger[] passengerDatarray=passengerData.getPassengersArray();
-				passengerBean = new PassengerBean();
-				if(passengerDatarray.length == 1){
-					passengerBean.setEmailAddress(passengerDatarray[0].getEmail());
-					passengerBean.setFirstName(passengerDatarray[0].getFirstname());
-					passengerBean.setLastName(passengerDatarray[0].getLastname());
-					passengerBean.setMiddleInitial(passengerDatarray[0].getMiddlename());
-					passengerBean.setPhone1(passengerDatarray[0].getHomephone());
-					passengerBean.setPhone2(passengerDatarray[0].getMobile());
-					passengerBean.setPhone4(passengerDatarray[0].getWorkphone());
-					passengerBean.setHotel(passengerDatarray[0].getHotel());
-				}
-				session.setAttribute("passengerBean", passengerBean);	
-				session.setAttribute("baggageState", baggageState);	
-				return "gotoDirectionPage";
-			} else {
-				clearCaptchaCache();
-				return null;
-			}
-
-		} else {
-			FacesUtil.addError("Incorrect Claim Number and Last Name combination. Please try again.");
-			logger.error("Claim Number and Last Name are incorrect for admin for the IP Adress: "
-							+((HttpServletRequest)FacesUtil.getFacesContext()
-							.getExternalContext().getRequest()).getRemoteAddr());
-			if (captchaBean.check().equalsIgnoreCase(CAPTCHA_STATUS)) {
-				captchaBean.setStatus("");
-			}
-			clearInputCache();
-			clearCaptchaCache();
-			return null;
-		}
-		 } catch (AxisFault e) {
-             e.printStackTrace();
-             return null;
-		 } catch (RemoteException e) {
-             e.printStackTrace();
-             return null;
-		 } catch (SimplePersistenceException e) {
-    	   e.printStackTrace();
-    	   	return null;
-	}
-	}
 	
 	/**
 	 * This is the 1st step for a passenger to fill up his claims form
@@ -193,8 +93,13 @@ public class PassengerController {
 		if (null != session && null != session.getAttribute("loggedPassenger")) {
 			try {
 				baggageState = (Long)session.getAttribute("baggageState");
+				String selectedLanguage=(String)session.getAttribute("selectedLanguage" );
 				passengerInfoLabel = passengerService.getPassengerInfo(selectedLanguage,baggageState);
-				passengerBean = (PassengerBean)session.getAttribute("passengerBean");
+				this.passengerBean = (PassengerBean)session.getAttribute("passengerBean");
+				List<CountryCode> countries = passengerService.getCountries();
+				for(CountryCode countryCode : countries){
+					selectItems.add(new SelectItem(countryCode.getId(), countryCode.getCountry()));
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -204,6 +109,39 @@ public class PassengerController {
 			return "passengerLogout";
 		}
 	}
+	
+	/**
+	 * Save the Passenger Data on passengerinfo page
+	 * 
+	 */
+	
+	public void savePassengerInfo(){
+		logger.debug("savePassengerInfo method is called");
+
+		HttpSession session = (HttpSession) FacesUtil.getFacesContext()
+				.getExternalContext().getSession(false);
+		if (null != session && null != session.getAttribute("loggedPassenger")) {
+			try{
+				//passengerBean = (PassengerBean)session.getAttribute("passengerBean");
+				boolean saveData=onlineClaimsWS.savePassengerInfo(passengerBean);
+				if(saveData){
+					FacesUtil.addInfo("Passenger infomation saved successfully.");
+					logger.info("Passenger infomation saved successfully.");
+				}else{
+					logger.error("Error in persisting the Data");
+					FacesUtil.addError("Error in persisting the Data");
+				}
+			} catch (AxisFault e) {
+				e.printStackTrace();
+				FacesUtil.addError("Error in persisting the Data");
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				FacesUtil.addError("Connection failure, Please try again");
+			}
+		} else {
+			FacesUtil.addError("Your session has been expired. Please log in again");
+		}
+	}
 
 	/**
 	 * Reset all the input fields to their default values.
@@ -211,7 +149,12 @@ public class PassengerController {
 	 * 
 	 */
 	public String cancel(){
-		//reset functionality to be implemented
+		FacesContext context = FacesUtil.getFacesContext();
+		  ViewHandler viewHandler = context.getApplication().getViewHandler();
+		  UIViewRoot viewRoot = viewHandler.createView(context,
+		  context.getViewRoot().getViewId()); context.setViewRoot(viewRoot);
+		  context.renderResponse(); // Optional
+		 
 		return null;
 	}
 	
@@ -227,8 +170,14 @@ public class PassengerController {
 				.getExternalContext().getSession(false);
 		if (null != session && null != session.getAttribute("loggedPassenger")) {
 			try {
-				flightLabel = passengerService.getFlightLabels(selectedLanguage);
-				passengerBean = (PassengerBean)session.getAttribute("passengerBean");
+				baggageState = (Long)session.getAttribute("baggageState");
+				String selectedLanguage=(String)session.getAttribute("selectedLanguage" );
+				flightLabel = passengerService.getFlightLabels(selectedLanguage,baggageState);
+				//passengerBean = (PassengerBean)session.getAttribute("passengerBean");
+				//if(0 == passengerBean.getBagTagList().size()){
+					//passengerBean.getBagTagList().add(new HtmlBagComponent());
+				//}
+				//session.setAttribute("passengerBean", passengerBean);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -238,6 +187,43 @@ public class PassengerController {
 			FacesUtil
 					.addError("Your session has been expired. Please log in again");
 			return "passengerLogout";
+		}
+	}
+	
+	/**
+	 * Calling this method at selection of the no of baggage
+	 * 
+	 * @param valueChangeEvent
+	 */
+	public void bagSelectListener(ValueChangeEvent valueChangeEvent){
+		logger.info("bagSelectListener method");
+		Integer noOfBags=(Integer)valueChangeEvent.getNewValue();
+		if(noOfBags > 0){
+			lostBagItems.clear();
+			List<Bag> bagList= new ArrayList<Bag>();
+			for(int i=1; i <= noOfBags;i++){
+				lostBagItems.add(new SelectItem(new Integer(i)));
+				Bag bag=new Bag();
+				bagList.add(bag);
+				passengerBean.setBagTagList(bagList);
+			}
+		}
+	}
+	
+	
+	public void rerouteBagListener(ValueChangeEvent valueChangeEvent){
+		logger.info("rerouteBagListener method");
+		Boolean rerouteBag=(Boolean)valueChangeEvent.getNewValue();
+		if(rerouteBag){
+			differentClaimCheckView=false; //readonly =true
+		}
+	}
+	
+	public void differentClaimCheckListener(ValueChangeEvent valueChangeEvent){
+		logger.info("differentClaimCheckListener method");
+		Boolean diffClaimCheck=(Boolean)valueChangeEvent.getNewValue();
+		if(diffClaimCheck){
+			differentClaimCheckView=false; //readonly =true
 		}
 	}
 
@@ -314,84 +300,64 @@ public class PassengerController {
 	public Map<String, List<Localetext>> getPageMaps() {
 		return pageMaps;
 	}
-
 	public void setPageMaps(Map<String, List<Localetext>> pageMaps) {
 		this.pageMaps = pageMaps;
 	}
-
 	public List<Localetext> getLoginPageList() {
 		return loginPageList;
 	}
-
 	public void setLoginPageList(List<Localetext> loginPageList) {
 		this.loginPageList = loginPageList;
 	}
-
-
-	public String getSelectedLanguage() {
-		return selectedLanguage;
-	}
-
-
-	public void setSelectedLanguage(String selectedLanguage) {
-		this.selectedLanguage = selectedLanguage;
-	}
-
-
 	public Set<SelectItem> getLanguageDropDown() {
 		return languageDropDown;
 	}
-
-
 	public void setLanguageDropDown(Set<SelectItem> languageDropDown) {
 		this.languageDropDown = languageDropDown;
 	}
-
-
-
-
 	public List<Localetext> getPassengerDirectionList() {
 		return passengerDirectionList;
 	}
-
-
-
-
 	public MultilingualLabel getPassengerInfoLabel() {
 		return passengerInfoLabel;
 	}
-
-
-
-
 	public MultilingualLabel getFlightLabel() {
 		return flightLabel;
 	}
-
-
-
-
 	public void setFlightLabel(MultilingualLabel flightLabel) {
 		this.flightLabel = flightLabel;
 	}
-
-
-
-
 	public PassengerBean getPassengerBean() {
 		return passengerBean;
 	}
-
-
-
-
 	public void setPassengerBean(PassengerBean passengerBean) {
 		this.passengerBean = passengerBean;
 	}
+	public List<SelectItem> getSelectItems() {
+		return selectItems;
+	}
+	public void setSelectItems(List<SelectItem> selectItems) {
+		this.selectItems = selectItems;
+	}
+	
+	public Set<SelectItem> getLostBagItems() {
+		return lostBagItems;
+	}
 
+	public void setLostBagItems(Set<SelectItem> lostBagItems) {
+		this.lostBagItems = lostBagItems;
+	}
 
+	public boolean getDifferentClaimCheckView() {
+		return differentClaimCheckView;
+	}
+	public void setDifferentClaimCheckView(boolean differentClaimCheckView) {
+		this.differentClaimCheckView = differentClaimCheckView;
+	}
 
+	public void setOnlineClaimsWS(OnlineClaimsWS onlineClaimsWS) {
+		this.onlineClaimsWS = onlineClaimsWS;
+	}
 	
 	
-
 }
---
+++ onlineclaims/src/main/java/com/nettracer/claims/passenger/controller/PassengerLoginController.java
@@ -0,0 +1,306 @@
+package com.nettracer.claims.passenger.controller;
+
+import java.rmi.RemoteException;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.html.HtmlForm;
+import javax.faces.component.html.HtmlInputSecret;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.component.html.HtmlPanelGrid;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.axis2.AxisFault;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.bagnet.nettracer.ws.core.pojo.xsd.WSPVAdvancedIncident;
+import com.bagnet.nettracer.ws.onlineclaims.xsd.PassengerView;
+import com.nettracer.claims.admin.bootstrap.PassengerBootstrap;
+import com.nettracer.claims.core.exception.SimplePersistenceException;
+import com.nettracer.claims.core.model.Languages;
+import com.nettracer.claims.core.model.Localetext;
+import com.nettracer.claims.core.model.PassengerBean;
+import com.nettracer.claims.core.service.PassengerService;
+import com.nettracer.claims.faces.util.CaptchaBean;
+import com.nettracer.claims.faces.util.FacesUtil;
+import com.nettracer.claims.passenger.LoginBean;
+import com.nettracer.claims.passenger.SessionPassengerBean;
+import com.nettracer.claims.webservices.client.OnlineClaimsWS;
+
+/**
+ * @author Utpal Description: This is the main controller for all the managed
+ *         beans including Navigation rule and validation for the passenger
+ *         side.
+ */
+
+@Component
+@Scope("request")
+@Qualifier("passengerLoginController")
+public class PassengerLoginController {
+	private static Logger logger = Logger
+			.getLogger(PassengerLoginController.class);
+	private static final String CAPTCHA_STATUS = "Correct";
+
+	CaptchaBean captchaBean = new CaptchaBean();
+	LoginBean loginBean = new LoginBean();
+	// private List<Map<String, List<Localetext>>> pageMapsList ;
+	private Map<String, List<Localetext>> pageMaps;
+	private List<Localetext> loginPageList;
+	private String selectedLanguage = "English-US"; // holds the dropdown value
+													// for language selection
+	private Set<SelectItem> languageDropDown = new LinkedHashSet<SelectItem>();
+	private List<Localetext> passengerDirectionList;
+	private Long baggageState;
+	private PassengerBean passengerBean;
+	
+	/*
+	 * @Autowired AdminService adminService;
+	 */
+
+	@Autowired
+	PassengerService passengerService;
+	
+	@Autowired
+	OnlineClaimsWS onlineClaimsWS;
+
+
+	public PassengerLoginController() {
+		logger.info("PassengerController constructor");
+		List<Languages> languagesList = PassengerBootstrap
+				.getLanguageDropDown();
+		for (Languages language : languagesList) {
+			if (language.getActiveStatus() == true) {
+				languageDropDown.add(new SelectItem(language.getDescription()));
+			}
+		}
+		loginPageList = PassengerBootstrap.getLoginPageList();
+		logger
+				.info("Size of loginPageList inside PassengerController constructor= "
+						+ loginPageList.size());
+	}
+
+	public void languageSelectionListener(ValueChangeEvent valueChangeEvent) {
+		logger.info("Listener:languageSelectionListener is called");
+		try {
+			selectedLanguage = (String) valueChangeEvent.getNewValue();
+			FacesContext context = FacesUtil.getFacesContext();
+			HttpSession session = (HttpSession) context
+					.getExternalContext().getSession(false);
+			session.setAttribute("selectedLanguage", selectedLanguage);
+			/*
+			 * HttpSession session = (HttpSession) FacesUtil.getFacesContext()
+			 * .getExternalContext().getSession(false);
+			 */
+			loginPageList = passengerService
+					.getPassengerLoginContents(selectedLanguage);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * If the validation would successful for login page then navigate to 2nd
+	 * page. i.e. the landing page
+	 * 
+	 * @return String
+	 * @throws AxisFault 
+	 */
+	public String gotoDirectionPage() {
+		logger.debug("gotoDirectionPage method is called");
+		 try {
+			PassengerView passengerView= onlineClaimsWS.getPassengerView(loginBean.getClaimNumber(), loginBean.getLastName());
+		
+		if (passengerView.getAuthenticationSuccess()) {
+			if (captchaBean.check().equalsIgnoreCase(CAPTCHA_STATUS)) {
+				FacesContext context = FacesUtil.getFacesContext();
+				HttpSession session = (HttpSession) context
+						.getExternalContext().getSession(false);
+				SessionPassengerBean sessionPassengerBean = (SessionPassengerBean) session
+						.getAttribute("sessionPassengerBean");
+				sessionPassengerBean.setLogoutRenderer(true);
+				session.setAttribute("sessionPassengerBean", sessionPassengerBean);
+				session.setAttribute("loggedPassenger", "loggedPassenger");
+				
+				baggageState=passengerView.getClaimId();
+				passengerDirectionList=passengerService.getPassengerDirection(selectedLanguage);
+				session.setAttribute("passengerDirectionList", passengerDirectionList);
+				
+				WSPVAdvancedIncident passengerData=passengerView.getData();
+				
+				passengerBean = onlineClaimsWS.getPassengerData(passengerData);
+				
+				session.setAttribute("passengerBean", passengerBean);	
+				session.setAttribute("baggageState", baggageState);	
+				session.setAttribute("selectedLanguage", selectedLanguage);	
+				return "gotoDirectionPage";
+			} else {
+				clearCaptchaCache();
+				return null;
+			}
+
+		} else {
+			FacesUtil.addError("Incorrect Claim Number and Last Name combination. Please try again.");
+			logger.error("Claim Number and Last Name are incorrect for admin for the IP Adress: "
+							+((HttpServletRequest)FacesUtil.getFacesContext()
+							.getExternalContext().getRequest()).getRemoteAddr());
+			if (captchaBean.check().equalsIgnoreCase(CAPTCHA_STATUS)) {
+				captchaBean.setStatus("");
+			}
+			clearInputCache();
+			clearCaptchaCache();
+			return null;
+		}
+		 } catch (AxisFault e) {
+			 logger.error("AxisFault Error");
+			 FacesUtil.addError("There is a Problem with the webservices, Please try again");
+            e.printStackTrace();
+            return null;
+		 } catch (RemoteException e) {
+			 logger.error("Error:RemoteException");
+            e.printStackTrace();
+            return null;
+		 } catch (SimplePersistenceException e) {
+   	   e.printStackTrace();
+   	   	return null;
+	}
+	}
+
+	
+
+	/*
+	 * Clear the browser cache(component value) from Apply request value phase
+	 */
+	public void clearCaptchaCache() {
+		logger
+				.debug("clearCaptchaCache method is called to clear the wrong captcha input texts");
+		FacesContext context = FacesUtil.getFacesContext();
+		/*
+		 * ViewHandler viewHandler = context.getApplication().getViewHandler();
+		 * UIViewRoot viewRoot = viewHandler.createView(context,
+		 * context.getViewRoot().getViewId()); context.setViewRoot(viewRoot);
+		 * context.renderResponse(); // Optional
+		 */
+		UIViewRoot viewRoot = context.getViewRoot();
+		HtmlInputText inputText = null;
+		HtmlForm htmlForm = (HtmlForm) viewRoot
+				.findComponent("loginPassengerForm");
+		HtmlPanelGrid htmlPanelGrid = (HtmlPanelGrid) htmlForm
+				.findComponent("loginPassengerPanel");
+
+		inputText = (HtmlInputText) htmlPanelGrid.findComponent("captcha");
+		inputText.setValue("");
+	}
+
+	/*
+	 * Clear the browser cache(component value) from Apply request value phase
+	 */
+	public void clearInputCache() {
+		logger
+				.debug("clearInputCache method is called to clear the wrong captcha input texts");
+		FacesContext context = FacesUtil.getFacesContext();
+		UIViewRoot viewRoot = context.getViewRoot();
+		HtmlInputText inputText = null;
+		HtmlForm htmlForm = (HtmlForm) viewRoot
+				.findComponent("loginPassengerForm");
+		HtmlPanelGrid htmlPanelGrid = (HtmlPanelGrid) htmlForm
+				.findComponent("loginPassengerPanel");
+
+		inputText = (HtmlInputText) htmlPanelGrid.findComponent("claimNumber");
+		if (null != inputText) {
+			inputText.setValue("");
+		}
+		HtmlInputText inputSecret = (HtmlInputText) htmlPanelGrid
+				.findComponent("lastName");
+		if (null != inputSecret) {
+			inputSecret.setValue("");
+		}
+	}
+
+	public String passengerLogout() {
+		return FacesUtil.passengerLogout();
+	}
+
+	public CaptchaBean getCaptchaBean() {
+		return captchaBean;
+	}
+
+	public void setCaptchaBean(CaptchaBean captchaBean) {
+		this.captchaBean = captchaBean;
+	}
+
+	public LoginBean getLoginBean() {
+		return loginBean;
+	}
+
+	public void setLoginBean(LoginBean loginBean) {
+		this.loginBean = loginBean;
+	}
+
+	public Map<String, List<Localetext>> getPageMaps() {
+		return pageMaps;
+	}
+
+	public void setPageMaps(Map<String, List<Localetext>> pageMaps) {
+		this.pageMaps = pageMaps;
+	}
+
+	public List<Localetext> getLoginPageList() {
+		return loginPageList;
+	}
+
+	public void setLoginPageList(List<Localetext> loginPageList) {
+		this.loginPageList = loginPageList;
+	}
+
+	public String getSelectedLanguage() {
+		return selectedLanguage;
+	}
+
+	public void setSelectedLanguage(String selectedLanguage) {
+		this.selectedLanguage = selectedLanguage;
+	}
+
+	public Set<SelectItem> getLanguageDropDown() {
+		return languageDropDown;
+	}
+
+	public void setLanguageDropDown(Set<SelectItem> languageDropDown) {
+		this.languageDropDown = languageDropDown;
+	}
+
+	public List<Localetext> getPassengerDirectionList() {
+		return passengerDirectionList;
+	}
+
+	public Long getBaggageState() {
+		return baggageState;
+	}
+
+	public void setBaggageState(Long baggageState) {
+		this.baggageState = baggageState;
+	}
+
+	public PassengerBean getPassengerBean() {
+		return passengerBean;
+	}
+
+	public void setPassengerBean(PassengerBean passengerBean) {
+		this.passengerBean = passengerBean;
+	}
+
+	
+
+
+}
