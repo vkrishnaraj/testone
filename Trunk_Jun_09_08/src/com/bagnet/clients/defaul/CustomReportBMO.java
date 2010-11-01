f73e6c6ba44734e169a46aa608bded1c9764bed8
ianyu <>
2010-11-01 16:21:02 +0000

    custom reporting bug fix -

--- Trunk_Jun_09_08/src/com/bagnet/clients/defaul/CustomReportBMO.java
+++ Trunk_Jun_09_08/src/com/bagnet/clients/defaul/CustomReportBMO.java
@@ -7,6 +7,11 @@ package com.bagnet.clients.defaul;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
+
+//import com.bagnet.clients.us.CustomReportBMO;
+import com.bagnet.nettracer.reporting.ReportingConstants;
+import com.bagnet.nettracer.tracing.bmo.ReportBMO;
 import com.bagnet.nettracer.tracing.db.Agent;
 import com.bagnet.nettracer.tracing.dto.StatReportDTO;
 
@@ -17,11 +22,23 @@ import com.bagnet.nettracer.tracing.dto.StatReportDTO;
  */
 public class CustomReportBMO implements
 		com.bagnet.nettracer.integrations.reports.CustomReportBMO {
-
+	
 	public String createCustomReport(StatReportDTO srDTO,
 			HttpServletRequest request, Agent user, String rootpath) {
 
-		return null;
+		String creportdata = null;
+		switch (srDTO.getCustomreportnum()) {
+		case ReportingConstants.RPT_20_CUSTOM_55:
+			creportdata = createDisputeResolutionReport(srDTO, ReportBMO.getCustomReport(55).getResource_key(), request, user);
+			break;
+		}
+		return creportdata;		
 	}
 
+	private String createDisputeResolutionReport(StatReportDTO srDTO, String resource_key, HttpServletRequest request, Agent user) {
+		
+		ReportBMO rbmo= new ReportBMO(request);
+		rbmo.setUser(user);
+		return rbmo.create_dispute_resolution_rpt(srDTO, 0, ReportingConstants.RPT_55_NAME, "Dispute Resolution Report");
+	}
 }
