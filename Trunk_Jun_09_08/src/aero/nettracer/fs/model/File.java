8c04ef620b2c3c1c811c089754dc3e29d3ae3060
byron <>
2011-04-22 16:51:56 +0000

    Incomplete - task : Prevent Duplicates

--- Trunk_Jun_09_08/src/aero/nettracer/fs/model/File.java
+++ Trunk_Jun_09_08/src/aero/nettracer/fs/model/File.java
@@ -1,6 +1,7 @@
 package aero.nettracer.fs.model;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -12,12 +13,22 @@ import javax.persistence.Transient;
 
 import org.hibernate.annotations.Proxy;
 
-
 @Entity
 @Proxy(lazy = false)
 public class File implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
+
+	@Transient
+	HashMap<Long, Double> matchingFiles = null;
+
+	public HashMap<Long, Double> getMatchingFiles() {
+		return matchingFiles;
+	}
+
+	public void setMatchingFiles(HashMap<Long, Double> matchingFiles) {
+		this.matchingFiles = matchingFiles;
+	}
 
 	@Transient
 	Set<Person> personCache = null;
@@ -26,77 +37,81 @@ public class File implements Serializable {
 	@Transient
 	Set<Phone> phoneCache = null;
 
-
-	
-
 	@Id
 	@GeneratedValue
 	private long id;
-	
+
 	private long swapId;
-	
-	@OneToOne(targetEntity = aero.nettracer.fs.model.FsClaim.class, cascade = CascadeType.ALL, mappedBy="file")
+
+	@OneToOne(targetEntity = aero.nettracer.fs.model.FsClaim.class, cascade = CascadeType.ALL, mappedBy = "file")
 	private FsClaim claim;
-	
-	@OneToOne(targetEntity = aero.nettracer.fs.model.FsIncident.class, cascade = CascadeType.ALL, mappedBy="file")
+
+	@OneToOne(targetEntity = aero.nettracer.fs.model.FsIncident.class, cascade = CascadeType.ALL, mappedBy = "file")
 	private FsIncident incident;
-	
-	public File(){
+
+	public File() {
 	}
-	
-	public File(long id){
+
+	public File(long id) {
 		this.id = id;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public FsClaim getClaim() {
 		return claim;
 	}
+
 	public void setClaim(FsClaim claim) {
 		this.claim = claim;
 	}
+
 	public FsIncident getIncident() {
 		return incident;
 	}
+
 	public void setIncident(FsIncident incident) {
 		this.incident = incident;
 	}
+
 	public void setSwapId(long swapId) {
 		this.swapId = swapId;
 	}
+
 	public long getSwapId() {
 		return swapId;
 	}
 
 	public Set<Person> getPersonCache() {
-  	return personCache;
-  }
+		return personCache;
+	}
 
 	public void setPersonCache(Set<Person> personCache) {
-  	this.personCache = personCache;
-  }
+		this.personCache = personCache;
+	}
 
 	public Set<FsAddress> getAddressCache() {
-  	return addressCache;
-  }
+		return addressCache;
+	}
 
 	public void setAddressCache(Set<FsAddress> addressCache) {
-  	this.addressCache = addressCache;
-  }
+		this.addressCache = addressCache;
+	}
 
 	public Set<Phone> getPhoneCache() {
-  	return phoneCache;
-  }
+		return phoneCache;
+	}
 
 	public void setPhoneCache(Set<Phone> phoneCache) {
-  	this.phoneCache = phoneCache;
-  }
-	
+		this.phoneCache = phoneCache;
+	}
+
 	public void resetCache() {
 		this.addressCache = null;
 		this.phoneCache = null;
--- Trunk_Jun_09_08/test/fs/ImportIncidentThread.java
+++ Trunk_Jun_09_08/test/fs/ImportIncidentThread.java
@@ -1,82 +1,82 @@
-package fs;
-
-import java.util.Date;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-
-public class ImportIncidentThread implements Runnable {
-	ArrayBlockingQueue<String> queue = null;
-	int totalSize = 0;
-	String hibernateAirline;
-	SessionFactory fraudFactory;
-	SessionFactory sessFactory;
-	Date startTime = null;
-	ConcurrentHashMap<String, Integer> completed = null;
-	public static Integer ref = new Integer(0);
-	private int myCount = 0;
-
-	public ImportIncidentThread(ArrayBlockingQueue<String> queue, int totalSize, String hibernateAirline, SessionFactory sess, SessionFactory fraudFactory, Date startTime,
-	    ConcurrentHashMap<String, Integer> completed) {
-		this.startTime = startTime;
-		this.queue = queue;
-		this.totalSize = totalSize;
-		this.hibernateAirline = hibernateAirline;
-		this.fraudFactory = fraudFactory;
-		this.sessFactory = sess;
-		this.completed = completed;
-	}
-
-	@Override
-	public void run() {
-		Session sess = null;
-		Session fraudSess = null;
-
-		while (true) {
-
-			if (sess == null) {
-				sess = sessFactory.openSession();
-				fraudSess = fraudFactory.openSession();
-			}
-
-			try {
-
-				String incidentId = queue.take();
-				myCount++;
-				try {
-					
-					ImportIncident.importIncident(incidentId, sess, fraudSess);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				completed.put(incidentId, ref);
-				int i = completed.size();
-				if (i % 50 == 0) {
-					Date nowTime = new Date();
-					System.out.println(incidentId);
-					double percComplete = ((double) i / (double) totalSize * 100);
-					long timeElapsed = (nowTime.getTime() - startTime.getTime()) / 1000;
-					double percentRemaining = 100 - percComplete;
-					double secondsToComplete = (double) timeElapsed / percComplete * percentRemaining;
-					System.out.println(hibernateAirline + " percent complete: " + (i / totalSize * 100) + " (" + i + "/" + totalSize + ")  Minutes remaining: " + secondsToComplete / 60);
-					
-					if (i % 200 == 0) {
-						sess.close();
-						sess = null;
-						fraudSess.close();
-						fraudSess = null;
-						myCount = 0;
-						System.out.println("Cleansing Thread's Hibernate Sessions...");
-					}
-				}
-
-			} catch (Exception ex) {
-
-			}
-
-		}
-
-	}
-}
+//package fs;
+//
+//import java.util.Date;
+//import java.util.concurrent.ArrayBlockingQueue;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//
+//public class ImportIncidentThread implements Runnable {
+//	ArrayBlockingQueue<String> queue = null;
+//	int totalSize = 0;
+//	String hibernateAirline;
+//	SessionFactory fraudFactory;
+//	SessionFactory sessFactory;
+//	Date startTime = null;
+//	ConcurrentHashMap<String, Integer> completed = null;
+//	public static Integer ref = new Integer(0);
+//	private int myCount = 0;
+//
+//	public ImportIncidentThread(ArrayBlockingQueue<String> queue, int totalSize, String hibernateAirline, SessionFactory sess, SessionFactory fraudFactory, Date startTime,
+//	    ConcurrentHashMap<String, Integer> completed) {
+//		this.startTime = startTime;
+//		this.queue = queue;
+//		this.totalSize = totalSize;
+//		this.hibernateAirline = hibernateAirline;
+//		this.fraudFactory = fraudFactory;
+//		this.sessFactory = sess;
+//		this.completed = completed;
+//	}
+//
+//	@Override
+//	public void run() {
+//		Session sess = null;
+//		Session fraudSess = null;
+//
+//		while (true) {
+//
+//			if (sess == null) {
+//				sess = sessFactory.openSession();
+//				fraudSess = fraudFactory.openSession();
+//			}
+//
+//			try {
+//
+//				String incidentId = queue.take();
+//				myCount++;
+//				try {
+//					
+//					ImportIncident.importIncident(incidentId, sess, fraudSess);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//				completed.put(incidentId, ref);
+//				int i = completed.size();
+//				if (i % 50 == 0) {
+//					Date nowTime = new Date();
+//					System.out.println(incidentId);
+//					double percComplete = ((double) i / (double) totalSize * 100);
+//					long timeElapsed = (nowTime.getTime() - startTime.getTime()) / 1000;
+//					double percentRemaining = 100 - percComplete;
+//					double secondsToComplete = (double) timeElapsed / percComplete * percentRemaining;
+//					System.out.println(hibernateAirline + " percent complete: " + (i / totalSize * 100) + " (" + i + "/" + totalSize + ")  Minutes remaining: " + secondsToComplete / 60);
+//					
+//					if (i % 200 == 0) {
+//						sess.close();
+//						sess = null;
+//						fraudSess.close();
+//						fraudSess = null;
+//						myCount = 0;
+//						System.out.println("Cleansing Thread's Hibernate Sessions...");
+//					}
+//				}
+//
+//			} catch (Exception ex) {
+//
+//			}
+//
+//		}
+//
+//	}
+//}
