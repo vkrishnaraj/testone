ca3d7d054b4ba9bc0e96c2baad393932a5f0f2b8
Chris Gordon <cgordon@nettracer.aero>
2011-12-09 16:24:45 +0000

    Suite. CG

---
+++ Selenium_Project/src/aero/nettracer/web/defaul/testing/TestEverything.java
@@ -0,0 +1,53 @@
+package aero.nettracer.web.defaul.testing;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import aero.nettracer.web.azul.testing.actions.AD_Login;
+import aero.nettracer.web.azul.testing.actions.AD_LoginFailure;
+import aero.nettracer.web.azul.testing.actions.AD_LoginTest;
+import aero.nettracer.web.azul.testing.actions.AD_VerifyText_TaskManager;
+import aero.nettracer.web.defaul.testing.actions.Def_Logout;
+import aero.nettracer.web.defaul.testing.actions.Def_PasswordResetNTTest;
+import aero.nettracer.web.jetblue.testing.actions.B6_Login;
+import aero.nettracer.web.jetblue.testing.actions.B6_LoginFailure;
+import aero.nettracer.web.jetblue.testing.actions.B6_LoginTest;
+import aero.nettracer.web.jetblue.testing.actions.B6_VerifyText_TaskManager;
+import aero.nettracer.web.spirit.testing.actions.NK_Login;
+import aero.nettracer.web.spirit.testing.actions.NK_LoginFailure;
+import aero.nettracer.web.spirit.testing.actions.NK_LoginTest;
+import aero.nettracer.web.spirit.testing.actions.NK_VerifyText_TaskManager;
+import aero.nettracer.web.utility.SeleniumTestBrowserDefault;
+import aero.nettracer.web.westjet.testing.actions.WS_Login;
+import aero.nettracer.web.westjet.testing.actions.WS_LoginFailure;
+import aero.nettracer.web.westjet.testing.actions.WS_LoginTest;
+import aero.nettracer.web.westjet.testing.actions.WS_VerifyText_TaskManager;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	AD_Login.class, Def_Logout.class, AD_LoginFailure.class, AD_LoginTest.class, 
+	Def_PasswordResetNTTest.class, Def_Logout.class, AD_Login.class, AD_VerifyText_TaskManager.class,
+	B6_Login.class, Def_Logout.class, B6_LoginFailure.class, B6_LoginTest.class, 
+	Def_PasswordResetNTTest.class, Def_Logout.class, B6_Login.class, B6_VerifyText_TaskManager.class,
+	NK_Login.class, Def_Logout.class, NK_LoginFailure.class, NK_LoginTest.class, 
+	Def_PasswordResetNTTest.class, Def_Logout.class, NK_Login.class, NK_VerifyText_TaskManager.class,
+	WS_Login.class, Def_Logout.class, WS_LoginFailure.class, WS_LoginTest.class, 
+	Def_PasswordResetNTTest.class, Def_Logout.class, WS_Login.class, WS_VerifyText_TaskManager.class
+	})
+public class TestEverything { 
+	
+	@BeforeClass
+	public static void oneTimeSetUp() {
+		SeleniumTestBrowserDefault.initBrowser();
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() {
+		SeleniumTestBrowserDefault.stopBrowser();
+	}
+	
+}
