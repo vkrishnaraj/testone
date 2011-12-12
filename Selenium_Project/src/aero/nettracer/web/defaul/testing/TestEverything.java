c6e2ad9183b68b370ca2206a665ec9298829ddee
Chris Gordon <cgordon@nettracer.aero>
2011-12-12 22:32:27 +0000

    CG

--- Selenium_Project/src/aero/nettracer/web/defaul/testing/TestEverything.java
+++ Selenium_Project/src/aero/nettracer/web/defaul/testing/TestEverything.java
@@ -30,18 +30,18 @@ import aero.nettracer.web.westjet.testing.actions.WS_VerifyText_TaskManager;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-//	AD_Login.class, Def_Logout.class, AD_LoginFailure.class, AD_LoginTest.class, 
-//	Def_PasswordResetNTTest.class, Def_Logout.class, 
-//	AD_Login.class, AD_VerifyText_TaskManager.class,
-//	B6_Login.class, Def_Logout.class, B6_LoginFailure.class, B6_LoginTest.class, 
-//	Def_PasswordResetNTTest.class, Def_Logout.class, 
-//	B6_Login.class, B6_VerifyText_TaskManager.class,
-//	NK_Login.class, Def_Logout.class, NK_LoginFailure.class, NK_LoginTest.class, 
-//	Def_PasswordResetNTTest.class, Def_Logout.class, 
-//	NK_Login.class, NK_VerifyText_TaskManager.class,
-//	WS_Login.class, Def_Logout.class, WS_LoginFailure.class, WS_LoginTest.class, 
-//	Def_PasswordResetNTTest.class, Def_Logout.class, 
-	WS_Login.class, //WS_VerifyText_TaskManager.class,
+	AD_Login.class, Def_Logout.class, AD_LoginFailure.class, AD_LoginTest.class, 
+	Def_PasswordResetNTTest.class, Def_Logout.class, 
+	AD_Login.class, AD_VerifyText_TaskManager.class,
+	B6_Login.class, Def_Logout.class, B6_LoginFailure.class, B6_LoginTest.class, 
+	Def_PasswordResetNTTest.class, Def_Logout.class, 
+	B6_Login.class, B6_VerifyText_TaskManager.class,
+	NK_Login.class, Def_Logout.class, NK_LoginFailure.class, NK_LoginTest.class, 
+	Def_PasswordResetNTTest.class, Def_Logout.class, 
+	NK_Login.class, NK_VerifyText_TaskManager.class,
+	WS_Login.class, Def_Logout.class, WS_LoginFailure.class, WS_LoginTest.class, 
+	Def_PasswordResetNTTest.class, Def_Logout.class, 
+	WS_Login.class, WS_VerifyText_TaskManager.class,
 	WS_CreateLD_VerifyRequiredFields.class
 	})
 public class TestEverything { 
