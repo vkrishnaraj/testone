93629a460e5cf5cc135b484aba67cc51e9646a3b
Chris Gordon <cgordon@nettracer.aero>
2012-01-25 20:30:40 +0000

    Westjet Prepop Page Support. CG

--- Selenium_Project/src/aero/nettracer/web/westjet/testing/actions/nt/onhands/WS_CreateOHD_VerifyRequiredFields.java
+++ Selenium_Project/src/aero/nettracer/web/westjet/testing/actions/nt/onhands/WS_CreateOHD_VerifyRequiredFields.java
@@ -14,39 +14,47 @@ public class WS_CreateOHD_VerifyRequiredFields extends DefaultSeleneseTestCase {
 		waitForPageToLoadImproved();
 		if (checkNoErrorPage()) {
 			checkCopyrightAndQuestionMarks();
-			selenium.click("name=savetracing");
-			assertEquals("Colour is required.", selenium.getAlert());
-			selenium.select("name=bagColor", "label=BK - Black");
-			selenium.click("name=savetracing");
-			assertEquals("Type is required.", selenium.getAlert());
-			selenium.select("name=bagType", "label=22");
-			selenium.click("name=savetracing");
-			assertEquals("From/To is required.", selenium.getAlert());
-			selenium.type("name=itinerarylist[0].legfrom", "ATL");
-			selenium.type("name=itinerarylist[0].legto", "LAX");
-			selenium.click("name=savetracing");
-			assertEquals("Airline/Flight Number is required.", selenium.getAlert());
-			selenium.type("name=itinerarylist[0].flightnum", "123");
-			selenium.click("name=savetracing");
-			assertEquals("Departure Date is required.", selenium.getAlert());
-			selenium.click("id=calendar20");
-			selenium.click("link=Today");
-			selenium.click("name=savetracing");
+			selenium.click("name=skip_prepopulate");
 			waitForPageToLoadImproved();
 			if (checkNoErrorPage()) {
 				checkCopyrightAndQuestionMarks();
-				verifyTrue(selenium.isTextPresent("OHD has been submitted."));
-				String onhand_id = selenium.getText("//td[@id='middlecolumn']/table/tbody/tr/td/h1/p/a");
-				Settings.ONHAND_ID_WS = onhand_id;
-				System.out.println("WS: Onhand Created: " + Settings.ONHAND_ID_WS);
-				selenium.click("//td[@id='middlecolumn']/table/tbody/tr/td/h1/p/a");
+				selenium.click("name=savetracing");
+				assertEquals("Colour is required.", selenium.getAlert());
+				selenium.select("name=bagColor", "label=BK - Black");
+				selenium.click("name=savetracing");
+				assertEquals("Type is required.", selenium.getAlert());
+				selenium.select("name=bagType", "label=22");
+				selenium.click("name=savetracing");
+				assertEquals("From/To is required.", selenium.getAlert());
+				selenium.type("name=itinerarylist[0].legfrom", "ATL");
+				selenium.type("name=itinerarylist[0].legto", "LAX");
+				selenium.click("name=savetracing");
+				assertEquals("Airline/Flight Number is required.", selenium.getAlert());
+				selenium.type("name=itinerarylist[0].flightnum", "123");
+				selenium.click("name=savetracing");
+				assertEquals("Departure Date is required.", selenium.getAlert());
+				selenium.click("id=calendar20");
+				selenium.click("link=Today");
+				selenium.click("name=savetracing");
 				waitForPageToLoadImproved();
+				if (checkNoErrorPage()) {
+					checkCopyrightAndQuestionMarks();
+					verifyTrue(selenium.isTextPresent("OHD has been submitted."));
+					String onhand_id = selenium.getText("//td[@id='middlecolumn']/table/tbody/tr/td/h1/p/a");
+					Settings.ONHAND_ID_WS = onhand_id;
+					System.out.println("WS: Onhand Created: " + Settings.ONHAND_ID_WS);
+					selenium.click("//td[@id='middlecolumn']/table/tbody/tr/td/h1/p/a");
+					waitForPageToLoadImproved();
+				} else {
+					System.err.println("Create Onhand Success Page Failed To Load. Error Page Loaded Instead.");
+					verifyTrue(false);
+				}
 			} else {
-				System.err.println("Create Onhand Success Page Failed To Load. Error Page Loaded Instead.");
+				System.err.println("Create Onhand Page Failed To Load. Error Page Loaded Instead.");
 				verifyTrue(false);
 			}
 		} else {
-			System.err.println("Create Onhand Page Failed To Load. Error Page Loaded Instead.");
+			System.out.println("!!!!!!!!!!!!!!! - Prepopulate Lost/Delay Page Failed To Load. Error Page Loaded Instead. - !!!!!!!!!!!!!!!!!!");
 			verifyTrue(false);
 		}
 	}
