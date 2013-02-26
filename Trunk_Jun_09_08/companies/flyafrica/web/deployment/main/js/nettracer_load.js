829ddbc6e1d656c37fa5ab547972bb079754c2e6
Chris Gordon <cgordon@nettracer.aero>
2013-02-26 18:11:14 +0000

    SNI-4

---
+++ Trunk_Jun_09_08/companies/flyafrica/web/deployment/main/js/nettracer_load.js
@@ -0,0 +1,17 @@
+function getLoadingContent() {
+	jQuery('#dialog').dialog('option', 'buttons', {} );
+	var h = '<div style="margin-top: 50px; text-align: center">';
+	h += '<img width="64" height="64" src="/webjet/deployment/main/images/loading2.gif" /><br />';
+	h += '<h5>Loading...</h5>';
+	h += '</div>';
+	return h;
+}
+
+function getSavingContent() {
+	jQuery('#dialog').dialog('option', 'buttons', {} );
+	var h = '<div style="margin-top: 50px; text-align: center">';
+	h += '<img width="64" height="64" src="/webjet/deployment/main/images/loading2.gif" /><br />';
+	h += '<h5>Saving...</h5>';
+	h += '</div>';
+	return h;
+}
