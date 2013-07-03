17af94a74849ec10283d7cc6b92b5bfabb3ee670
Chris Gordon <cgordon@nettracer.aero>
2013-07-03 23:37:18 +0000

    radixx wsdls

---
+++ Axis15/bin/Radixxwsdl2j.bat
@@ -0,0 +1,3 @@
+call wsdl2Java -uri Radixx_ConnectPoint_Reservation.wsdl -ss -sd -g -d xmlbeans -o service
+call wsdl2Java -uri Radixx_ConnectPoint_Security.wsdl -ss -sd -g -d xmlbeans -o service
+call wsdl2Java -uri radixxbooking.wsdl -ss -sd -g -d xmlbeans -o service
