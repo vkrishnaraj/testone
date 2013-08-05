dfbb8b19683bfeaff065a61a0ff8b7f4350b6001
Mike Sanders <msanders@nettracer.aero>
2013-08-05 16:30:13 +0000

    NT-727: updated the test port for Southwest.

--- Selenium_Project/src/aero/nettracer/web/defaul/testing/TestSouthwest.java
+++ Selenium_Project/src/aero/nettracer/web/defaul/testing/TestSouthwest.java
@@ -11,6 +11,7 @@ import aero.nettracer.web.southwest.testing.actions.nt.core.WN_Login;
 import aero.nettracer.web.southwest.testing.actions.nt.incidents.lostdelay.WN_CreateLD_VerifyRequiredFields;
 import aero.nettracer.web.southwest.testing.actions.nt.onhand.WN_CreateOhd;
 import aero.nettracer.web.utility.SeleniumTestBrowserDefault;
+import aero.nettracer.web.utility.Settings;
 
 @RunWith(Suite.class)
 @SuiteClasses({
@@ -20,7 +21,7 @@ public class TestSouthwest {
 	
 	@BeforeClass
 	public static void oneTimeSetUp() {
-		SeleniumTestBrowserDefault.initBrowser();
+		SeleniumTestBrowserDefault.initBrowser(Settings.PORT3);
 	}
 	
 	@AfterClass
