73fb11e9e0432025d90663382560724426e0bcc2
Sean Fine <sfine@nettracer.aero>
2013-08-07 20:57:42 +0000

    NT-713 tests

--- Selenium_Project/src/aero/nettracer/web/southwest/testing/actions/nt/onhands/WN_OHD_Sort_Search.java
+++ Selenium_Project/src/aero/nettracer/web/southwest/testing/actions/nt/onhands/WN_OHD_Sort_Search.java
@@ -8,32 +8,157 @@ public class WN_OHD_Sort_Search extends DefaultSeleneseTestCase {
 
 	@Test
 	public void testSortText() throws Exception {
-		// MJS: initial state is drivers license collection enabled
-		// 		and view/edit drivers license disabled.
-
-
+		//TODO: Create OnHand Code and Use the ID (unable to guarantee it's use on a constant basis)
+		
 		selenium.click("id=menucol_4.4");
 		waitForPageToLoadImproved();
-		selenium.click("id=button");
-		waitForPageToLoadImproved();
-		verifyEquals("//td[@id='middlecolumn']/div/table[2]/tbody/tr[2]/td", "LZWN000000006");
-		selenium.click("link=Color");
-		waitForPageToLoadImproved();
-		verifyNotEquals("//td[@id='middlecolumn']/div/table[2]/tbody/tr[2]/td", "LZWN000000006");
-		selenium.click("link=Bag Tag Number");
-		waitForPageToLoadImproved();
-		verifyEquals("//td[@id='middlecolumn']/div/table[2]/tbody/tr[2]/td", "LZWN000000006");
-		selenium.click("link=Name");
-		waitForPageToLoadImproved();
-		verifyNotEquals("//td[@id='middlecolumn']/div/table[2]/tbody/tr[2]/td", "LZWN000000006");
-		selenium.click("link=Type");
-		waitForPageToLoadImproved();
-		verifyEquals("//td[@id='middlecolumn']/div/table[2]/tbody/tr[2]/td", "LZWN000000006");
-		selenium.click("link=On-hand Number");
-		waitForPageToLoadImproved();
-		verifyEquals("//td[@id='middlecolumn']/div/table[2]/tbody/tr[2]/td", "LZWN000000006");
-		goToTaskManager();
-		waitForPageToLoadImproved();
+		
+		if(checkNoErrorPage()) {
+			selenium.click("id=button");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to reach OHD Search page - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=On-hand Number");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to search for OHDs  - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=Color");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to Sort By On-Hand Number  - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=Color");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to Sort By Color  - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=Bag Tag Number");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Color - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=Bag Tag Number");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Bag Tag Number - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=Name");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Bag Tag Number - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=Name");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Name - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=Type");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Name - !!!!!!!!!!!!!!!!!!");
+		}
+
+		if(checkNoErrorPage()) {
+			selenium.click("link=Type");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Type - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("link=Status");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Type - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("link=Status");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Status - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("link=Airline Found");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Status - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("link=Airline Found");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Airline Found - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("id=foundStation");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Airline Found - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("id=foundStation");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Station Found - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("id=holdStation");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Station Found - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("id=holdStation");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Holding Station - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("link=OHD Date/Time");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on Holding Station - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			selenium.click("link=OHD Date/Time");
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on OHD Date - !!!!!!!!!!!!!!!!!!");
+		}
+		
+		if(checkNoErrorPage()) {
+			goToTaskManager();
+			waitForPageToLoadImproved();
+		} else {
+			System.out.println("!!!!!!!!!!!!!!! - Failed to sort on OHD Date - !!!!!!!!!!!!!!!!!!");
+		}
 		
 	}
 }
