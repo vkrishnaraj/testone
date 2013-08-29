8b53ba20f317270628fd8e22590333fe9c3564ea
Sean Fine <sfine@nettracer.aero>
2013-08-29 19:00:19 +0000

    NT-725

--- Trunk_Jun_09_08/test/com/bagnet/nettracer/cronjob/utilities/CronUtilsTest.java
+++ Trunk_Jun_09_08/test/com/bagnet/nettracer/cronjob/utilities/CronUtilsTest.java
@@ -4,6 +4,7 @@ package com.bagnet.nettracer.cronjob.utilities;
 import org.hibernate.Session;
 import org.junit.Test;
 
+import com.bagnet.nettracer.cronjob.incident.AutoClose;
 import com.bagnet.nettracer.hibernate.*;
 
 
@@ -12,5 +13,11 @@ public class CronUtilsTest {
 	public void hourlySqlServerStatusCheckTest(){
 		CronUtils CU = new CronUtils("B6");
 		CU.hourlySqlServerStatusCheck();
+	}
+	
+	@Test
+	public void testCloseOHDS(){
+		AutoClose ac=new AutoClose("WN");
+		ac.autoCloseOHDs();
 	}
 }
