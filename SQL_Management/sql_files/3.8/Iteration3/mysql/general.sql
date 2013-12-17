eb1cbaf396d42d5f150348434c07fbc310b989ec
Mike Sanders <msanders@nettracer.aero>
2013-12-17 13:43:04 +0000

    NT-744: added permission for customer communication approval queue

--- SQL_Management/sql_files/3.8/Iteration3/mysql/general.sql
+++ SQL_Management/sql_files/3.8/Iteration3/mysql/general.sql
@@ -280,6 +280,8 @@ alter table expensepayout add column printcount int default 0;
 alter table expensetype modify description varchar(30); 
 
 insert into systemcomponents (component_id,component_name,component_desc,parent_component_id,component_action_link,display,sort_order,sort_group) VALUES (
+   1101,'Cust_Comm_Approval_Queue','Manage approval queue for customer communiations.',15,'customerCommunicationsApp.do',1,99,3);
+insert into systemcomponents (component_id,component_name,component_desc,parent_component_id,component_action_link,display,sort_order,sort_group) VALUES (
    1102,'Cust_Comm_Rejection_Queue','Manage rejection queue for customer communiations.',15,'customerCommunicationsRejected.do',1,100,3);
 
 alter table task add incidentActivityId bigint default null;
