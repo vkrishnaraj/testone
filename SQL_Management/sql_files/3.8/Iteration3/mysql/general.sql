0a0ab19999506d048fd4ee0c5757cf7ef108c7ff
Steve Lin <slin@nettracer.aero>
2014-01-14 19:06:33 +0000

    NT 1920 move PassengerExp script to iteration 4 per Sean

--- SQL_Management/sql_files/3.8/Iteration3/mysql/general.sql
+++ SQL_Management/sql_files/3.8/Iteration3/mysql/general.sql
@@ -472,23 +472,3 @@ create table inboundqueue(
     primary key (id));
     
 alter table issuance_category add limitByPassenger bit not null default 0;
-
-CREATE TABLE `passengerexp` (   
-`PassengerExp_ID` int(11) NOT NULL AUTO_INCREMENT,   
-`expensepayout_ID` varchar(13) NOT NULL DEFAULT '',   
-`firstname` varchar(20) DEFAULT NULL,   
-`middlename` varchar(20) DEFAULT NULL,   
-`lastname` varchar(20) DEFAULT NULL,  
-`address1` varchar(50) DEFAULT NULL,   
-`address2` varchar(50) DEFAULT NULL,   
-`city` varchar(50) DEFAULT NULL,   
-`state_ID` varchar(2) DEFAULT NULL,   
-`province` varchar(100) DEFAULT '',   
-`zip` varchar(11) DEFAULT NULL,   
-`homephone` varchar(50) DEFAULT NULL,
-`workphone` varchar(50) DEFAULT NULL,
-`mobile` varchar(50) DEFAULT NULL,
-`email` varchar(100) DEFAULT NULL,     
-`countrycode_ID` varchar(3) DEFAULT NULL, 
-PRIMARY KEY (`PassengerExp_ID`),   
-KEY `expensepayout_ID` (`expensepayout_ID`) ) ;
