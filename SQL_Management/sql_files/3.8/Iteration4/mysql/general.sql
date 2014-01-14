1eaef86c73c705da2c425e1ceac0a53e97ff8499
Sean Fine <sfine@nettracer.aero>
2014-01-14 18:04:49 +0000

    NT-1916 sql

---
+++ SQL_Management/sql_files/3.8/Iteration4/mysql/general.sql
@@ -0,0 +1,10 @@
+insert into template_var (displayTag,associatedClass) VALUES 
+   ('FullAddress' ,'Incident'), 
+   ('FullAddress','Claim'),
+   ('FullAddress','FoundItem'),
+   ('Country','Incident'), 
+   ('Country','Claim'), 
+   ('Country','FoundItem'),
+   ('Province','Incident'), 
+   ('Province','Claim'), 
+   ('Province','FoundItem');
