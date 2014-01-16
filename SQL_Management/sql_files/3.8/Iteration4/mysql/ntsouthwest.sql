8c629fe32a2269bb0b21b18ef40c9e693246e903
Chris Gordon <cgordon@nettracer.aero>
2014-01-16 18:26:25 +0000

    NT-1969

--- SQL_Management/sql_files/3.8/Iteration4/mysql/ntsouthwest.sql
+++ SQL_Management/sql_files/3.8/Iteration4/mysql/ntsouthwest.sql
@@ -1,2 +1,5 @@
 insert into properties (keyStr, valueStr) values ('bso.expense.process',1);
 insert into properties (keystr, valuestr) values ('sso.auto.provision', 1);
+insert into properties (keyStr, valueStr) values ('populate.itinerary.dateonly',1);
+update properties set valueStr = '36' where keyStr = 'reservation.hours.backward';
+update properties set valueStr = '12' where keyStr = 'reservation.hours.forward';
