1546dcdc4f7189b1a02227af765bcc85ea4358f8
Chris Gordon <cgordon@nettracer.aero>
2014-01-23 19:52:09 +0000

    NT-1925 (Removing passengerexp because it was removed as part of NT-1920)

--- SQL_Management/sql_files/3.8/Iteration4/Report Queries/1925-CSSReporting-Reservations-LUV-Voucher-Report-ORC-Weekly.sql
+++ SQL_Management/sql_files/3.8/Iteration4/Report Queries/1925-CSSReporting-Reservations-LUV-Voucher-Report-ORC-Weekly.sql
@@ -5,13 +5,12 @@ date_format(ep.createdate,'%m/%d/%y') as issueDate,
 ifnull(ep.ordernum,'') as orderNum, 
 concat(a.firstname,' ',a.lastname) as issueAgent, 
 s.stationcode as location, 
-concat(p.firstname,' ',p.lastname) as customerName, 
+concat(ep.firstname,' ',ep.lastname) as customerName, 
 format(ep.voucheramt, 2) as amount
 
 from expensepayout ep
 left outer join agent a on a.Agent_ID = ep.agent_ID
 left outer join station s on s.Station_ID = a.station_ID
-left outer join passengerexp p on p.expensepayout_ID = ep.Expensepayout_ID
 
 where 
 ep.paytype = 'VOUCH' and 
