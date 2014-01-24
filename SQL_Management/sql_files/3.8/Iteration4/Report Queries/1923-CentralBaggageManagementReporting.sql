df8beaf11a1c8613717602d23ccd389df45e013c
Sean Fine <sfine@nettracer.aero>
2014-01-24 21:08:25 +0000

    NT-1923 - Detailing variables and updates based on PO Review

--- SQL_Management/sql_files/3.8/Iteration4/Report Queries/1923-CentralBaggageManagementReporting.sql
+++ SQL_Management/sql_files/3.8/Iteration4/Report Queries/1923-CentralBaggageManagementReporting.sql
@@ -1,7 +1,9 @@
 # Admin Activity Summary Report - Detail and Summary - Is based on and returns GMT Time. 
-#The query should return all the information needed for both Detail and Summary Reports. 
+#The query should return all the information needed for both Detail and Summary Reports and can be summed up based on data returned. 
 #The detail vs summary selection should mainly control how the values are summed up and displayed
-select count(ia.id), act.code, act.description, concat(a.firstname," ",a.lastname) as agentname, a.username
+#startDate - The beginning of the date range. DateTime variable
+#endDate - the end of the date range. DateTime variable
+select count(ia.id) as activityCount, act.code as activityCode, act.description as activityDescription, a.firstname,a.lastname, a.username
   from incident_activity ia 
     inner join activity act on ia.activity = act.id 
     inner join agent a on ia.agent = a.Agent_ID 
@@ -11,57 +13,65 @@ select count(ia.id), act.code, act.description, concat(a.firstname," ",a.lastnam
 #----------------------------------
 
 #Assigned Claims by Representative - Is based on and returns GMT Time
-select concat(a.firstname," ",a.lastname) as agentname, a.username, 
-  inc.Incident_ID, inc.createdate, (case inc.itemtype_ID when 1 then "Lost" when 2 then "Missing" when 3 then "Damage" else "" end) as type 
+#startDate - The beginning of the date range. DateTime variable
+#endDate - the end of the date range. DateTime variable
+select a.firstname,a.lastname, a.username, 
+  inc.Incident_ID, timestamp(inc.createdate,inc.createtime) as date, 
+  (case inc.itemtype_ID when 1 then "Lost" when 2 then "Missing" when 3 then "Damage" else "" end) as type 
     from incident inc 
     inner join agent a on inc.agentassigned_ID=a.Agent_ID 
-      where inc.createdate>=:startdate and inc.createdate<=:enddate 
+      where inc.createdate>=:startdate and inc.createdate<=:enddate and inc.status_ID=12
       order by a.firstname, (case inc.itemtype_ID when 1 then "Lost" when 2 then "Missing" when 3 then "Damage" else "" end);
+      
 #----------------------------------
 
 #Agent Audit Report -Is based on and returns GMT Time. Is a Procedure based on percentage requirements
-drop procedure getAgentAuditReport;
-DELIMITER //
-create procedure getAgentAuditReport (in startDate datetime, in endDate datetime, in agentlist varchar(100), in percent varchar(3))
-begin
-declare percentlimit int default 0;
-set percentlimit =(SELECT ceiling((COUNT(inc.incident_id)*percent/100)) FROM incident inc inner join agent a on inc.agentassigned_ID=a.Agent_ID 
-    inner join incident_Activity ia on ia.incident=inc.incident_id
-    inner join activity act ON ia.activity = act.id
-    left outer join Passenger p ON p.incident_ID = inc.Incident_ID 
-    where inc.createdate>=startDate and inc.createdate<=endDate and find_in_set(a.username, agentlist));
-select concat(a.firstname,' ',a.lastname) as agentname, a.username, inc.Incident_ID, inc.createdate,
-  act.code, act.description, concat(p.lastname,', ',p.firstname)
-  as custName from incident inc inner join agent a on inc.agentassigned_ID=a.Agent_ID 
-  inner join incident_Activity ia on ia.incident=inc.incident_id 
-  inner join activity act ON ia.activity = act.id 
-  left outer join Passenger p ON p.incident_ID = inc.Incident_ID 
-  where inc.createdate>=startDate and inc.createdate<=endDate
-  and find_in_set(a.username, agentlist) 
-  limit percentlimit;
-end //
+#startDate - The beginning of the date range. DateTime variable
+#endDate - the end of the date range. DateTime variable
+#agentlist - List of Agent Usernames to check against
+#percent - Percentage of results returned back
 call getAgentAuditReport (:startDate, :endDate, :agentlist,:percent);
 #----------------------------------
 
 #Inventory Productivity by Representative - Is based on and returns GMT Time
-select concat(a.firstname," ",a.lastname) agentname, o.OHD_ID, o.founddate, concat(o.color," ",o.type) as colorType, s.description as bagState
-  from ohd o inner join status s on s.Status_ID = o.status_ID inner join agent a 
-  on o.agent_ID = a.Agent_ID where founddate >=:startDate and founddate <=:endDate order by concat(a.firstname," ",a.lastname);
+#startDate - The beginning of the date range. DateTime variable
+#endDate - the end of the date range. DateTime variable
+select a.firstname,a.lastname,a.username, 
+  o.OHD_ID, o.founddate as date, o.color,o.type, 
+  (case when isNull(it.status_ID) or it.status_id='' or it.status_ID in (47) then "Not Found" when it.status_id in (48,49,56) 
+    then "Found" when it.status_id in (50,59) then "Returned" end) as itemState,
+  s.description as bagState
+  from ohd o 
+    inner join status s on s.Status_ID = o.status_ID
+    inner join status ds on ds.Status_ID = o.disposal_status_ID
+    inner join agent a on o.agent_ID = a.Agent_ID
+    left outer join item it on it.ohd_id= o.OHD_ID
+    where founddate >=:startDate and founddate <=:endDate order by a.firstname;
 #----------------------------------
-  
-#Report Claims by Agent (Summary) - Is based on and returns GMT Time
-select a.username,concat(a.firstname," ",a.lastname) as agentName, count(ep.Expensepayout_ID) as claimNum, sum(ep.checkamt+ep.voucheramt+ep.creditcard_refund) as total, round(sum(ep.checkamt+ep.voucheramt+ep.creditcard_refund)/count(ep.Expensepayout_ID),2) as average 
-from  expensepayout ep inner join agent a on ep.agent_ID = a.Agent_ID where ep.createdate>=:startDate and ep.createdate <=:endDate group by a.username;
+
+#Report Claims by Agent (Summary) - Is based on and returns GMT Time 
+#startDate - The beginning of the date range. DateTime variable
+#endDate - the end of the date range. DateTime variable
+select a.username,concat(a.firstname," ",a.lastname) as agentName, count(f.id) as claimCount, sum(f.amountPaid) as total, 
+round(sum(f.amountPaid)/count(f.ID),2) as average 
+	from  fsclaim f inner join incident i on i.Incident_ID=f.ntIncidentId inner join agent a on i.agentassigned_id = a.Agent_ID
+		where f.claimDate>=:startDate and f.claimDate <=:endDate group by a.username;
 
 #Report Claims by Agent (Detail) - Is based on and returns GMT Time 
-select a.username,concat(a.firstname," ",a.lastname) as agentName, ep.incident_ID, concat(ep.firstname," ",ep.lastname) as custName, 
-  (ep.checkamt+ep.voucheramt+ep.creditcard_refund) as total
-  from expensepayout ep 
-    inner join agent a on ep.agent_ID = a.Agent_ID 
-      where ep.createdate>=:startDate and ep.createdate <=:endDate and (ep.status_id = 53 or ep.status_id=55);
+#startDate - The beginning of the date range. DateTime variable
+#endDate - the end of the date range. DateTime variable
+select a.username,a.firstname,a.lastname, f.ntIncidentId, p.firstname, p.lastName,
+  (f.amountPaid) as total
+  from fsclaim f inner join incident i on i.Incident_ID=f.ntIncidentId 
+  inner join agent a on i.agentassigned_id = a.Agent_ID
+  inner join person p on p.claim_id=f.id
+      where f.claimDate>=:startDate and f.claimDate <=:endDate;
 #----------------------------------
 
 #CBS SLV Issuance Report - Is based on and returns GMT Time
+#startDate - The beginning of the date range. DateTime variable
+#endDate - the end of the date range. DateTime variable
+#agentlist - List of Agent Usernames to check against
 select concat(a.firstname," ",a.lastname) as agentName, ep.createdate, concat(ep.firstname," ",ep.lastname) as custName, ep.incident_id, inc.recordlocator, ep.paycode, 
 (case inc.itemtype_ID when 1 then "LOST" when 2 then "MISSING" when 3 then "DAMAGE" else "" end) as reportType,
 (case ep.status_id when 54 then "DENY" when 55 then "SETTLE" else "PENDING" end) as resolveType, ep.voucheramt
@@ -73,6 +83,9 @@ select concat(a.firstname," ",a.lastname) as agentName, ep.createdate, concat(ep
 #----------------------------------
 
 #Individual Activity Report - Is based on and returns GMT Time
+#startDate - The beginning of the date range. DateTime variable
+#endDate - the end of the date range. DateTime variable
+#agentlist - List of Agent Usernames to check against
 select concat(a.firstname," ",a.lastname) agentname, ia.createDate, i.Incident_ID, act.code, act.description, (case act.code when "55" then d.title when "55C" then d.title else "" end) as correspondenceType from incident_activity ia 
 inner join incident i on i.Incident_ID=ia.incident 
 inner join activity act on act.id = ia.activity
