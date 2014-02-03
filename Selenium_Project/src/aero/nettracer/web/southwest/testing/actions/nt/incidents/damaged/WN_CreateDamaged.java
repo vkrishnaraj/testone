52920c06d0775420504012e982a8d7f1b5391a85
Sean Fine <sfine@nettracer.aero>
2014-02-03 22:14:22 +0000

    Fixed Tests

--- Selenium_Project/src/aero/nettracer/web/southwest/testing/actions/nt/incidents/damaged/WN_CreateDamaged.java
+++ Selenium_Project/src/aero/nettracer/web/southwest/testing/actions/nt/incidents/damaged/WN_CreateDamaged.java
@@ -534,7 +534,6 @@ public class WN_CreateDamaged extends WN_SeleniumTest {
 			selenium.click("id=button");
 			assertEquals("Pickup Date Time is required.", selenium.getAlert());
 			selenium.type("name=disppickuptime","12:00");
-			selenium.check("name=theitem[0].noAddFees");
 			selenium.click("id=button");
 			assertEquals("Fault Code is required.", selenium.getAlert());
 			selenium.select("name=theitem[0].lossCode", "value=11");
