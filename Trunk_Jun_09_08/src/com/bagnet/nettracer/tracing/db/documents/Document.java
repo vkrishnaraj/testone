89e094536590e66c89cbe797dadb6ec56a023727
Chris Gordon <cgordon@nettracer.aero>
2014-02-24 18:59:01 +0000

    NT-2250

--- Trunk_Jun_09_08/src/com/bagnet/nettracer/tracing/db/documents/Document.java
+++ Trunk_Jun_09_08/src/com/bagnet/nettracer/tracing/db/documents/Document.java
@@ -82,7 +82,8 @@ public class Document {
 	}
 	
 	public String getXml() {
-		StringBuilder xml = new StringBuilder("<?xml version=\"1.0\"?><!DOCTYPE html [<!ENTITY nbsp \"&#160;\">]>");
+		StringBuilder xml = new StringBuilder("<?xml version=\"1.0\"?>");
+		xml.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
 		xml.append("<html><body>");
 		xml.append(getContent());
 		xml.append("</body></html>");
