9a3d414dd76e3a14f7c02f5480f76c651b5bbb86
Sean Fine <sfine@nettracer.aero>
2014-04-17 18:39:54 +0000

    NT-2281 - Initial SQL Commit

---
+++ SQL_Management/sql_files/3.9/mysql/Southwest.sql
@@ -0,0 +1,2 @@
+insert into properties (keyStr,valueStr) VALUES ('loss.code.bags.lock','1');
+insert into properties (keyStr,valueStr) VALUES ('loss.code.month.day.locked','5');
--- SQL_Management/sql_files/3.9/mysql/general.sql
+++ SQL_Management/sql_files/3.9/mysql/general.sql
@@ -1,1 +1,3 @@
 insert into template_var (displayTag,associatedClass) values ('Salutation','Incident');
+
+insert into systemcomponents (component_id,component_name,component_desc,parent_component_id,component_action_link,display,sort_order,sort_group) VALUES (689 ,'Edit Locked Loss Codes' ,'Allows User to Edit Loss Codes and Stations that are Locked' ,1100 ,'' ,0 ,99 ,0);
