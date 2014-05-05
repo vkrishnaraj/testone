886a6e3d18b71be72632fb33842cbd06a19b3aa7
Perry May <pmay@nettracer.aero>
2014-05-05 14:34:01 +0000

    NT-2567 - SWA Improvement 271 - Activities Dropdown List

--- SQL_Management/sql_files/3.9/mysql/Southwest.sql
+++ SQL_Management/sql_files/3.9/mysql/Southwest.sql
@@ -1,2 +1,13 @@
 insert into properties (keyStr,valueStr) VALUES ('loss.code.bags.lock','1');
 insert into properties (keyStr,valueStr) VALUES ('loss.code.month.day.locked','5');
+
+-- NT-2596
+update activity set description ='NETTRACER FS MATCH' where code = '601';
+update activity set description ='NETTRACER FS COPY RCVD' where code = '79';
+update activity set description ='NETTRACER FS SENT - AWAITING FILE(S)' where code = '00';
+
+insert into activity (code, description, display) values ( '96', 'INBOUND CLAIM FORM', 1);
+
+update activity set display=0 where code in ('9206', '107','288', '223', '592', '590', 'CBP2', 'CBP1', '580', '83', '500', '01F', '578', '579', '208', '210', '278', '301', '312', '224', '289', '221','597','220','238','235','222','302','252','244','232','311','315','112','94','01','102','570','563','562', '85V');
+
+update activity set description=concat(code, ' - ', description);
